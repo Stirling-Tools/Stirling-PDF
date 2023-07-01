5ea3bcc1dd246d7e79d578081f82b3984cf5939a
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2023-07-01 22:18:49 +0100

    watermark stuff

--- src/main/java/stirling/software/SPDF/config/CleanUrlInterceptor.java
+++ src/main/java/stirling/software/SPDF/config/CleanUrlInterceptor.java
@@ -1,4 +1,5 @@
 package stirling.software.SPDF.config;
+
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,56 +21,59 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
 public class CleanUrlInterceptor implements HandlerInterceptor {
 
+	private static final List<String> ALLOWED_PARAMS = Arrays.asList("lang", "endpoint", "endpoints");
 
-    private static final List<String> ALLOWED_PARAMS = Arrays.asList("lang", "endpoint", "endpoints");
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String queryString = request.getQueryString();
+		if (queryString != null && !queryString.isEmpty()) {
+			String requestURI = request.getRequestURI();
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String queryString = request.getQueryString();
-        if (queryString != null && !queryString.isEmpty()) {
-            String requestURI = request.getRequestURI();
+			Map<String, String> parameters = new HashMap<>();
 
-            Map<String, String> parameters = new HashMap<>();
+			// Keep only the allowed parameters
+			String[] queryParameters = queryString.split("&");
+			for (String param : queryParameters) {
+				String[] keyValue = param.split("=");
+				if (keyValue.length != 2) {
+					continue;
+				}
+				if (ALLOWED_PARAMS.contains(keyValue[0])) {
+					parameters.put(keyValue[0], keyValue[1]);
+				}
+			}
 
-            // Keep only the allowed parameters
-            String[] queryParameters = queryString.split("&");
-            for (String param : queryParameters) {
-                String[] keyValue = param.split("=");
-                if (keyValue.length != 2) {
-                    continue;
-                }
-                if (ALLOWED_PARAMS.contains(keyValue[0])) {
-                    parameters.put(keyValue[0], keyValue[1]);
-                }
-            }
+			// If there are any parameters that are not allowed
+			if (parameters.size() != queryParameters.length) {
+				// Construct new query string
+				StringBuilder newQueryString = new StringBuilder();
+				for (Map.Entry<String, String> entry : parameters.entrySet()) {
+					if (newQueryString.length() > 0) {
+						newQueryString.append("&");
+					}
+					newQueryString.append(entry.getKey()).append("=").append(entry.getValue());
+				}
 
-            // If there are any other query parameters besides the allowed ones
-            if (parameters.size() > 0) {
-                // Construct new query string
-                StringBuilder newQueryString = new StringBuilder();
-                for (Map.Entry<String, String> entry : parameters.entrySet()) {
-                    if (newQueryString.length() > 0) {
-                        newQueryString.append("&");
-                    }
-                    newQueryString.append(entry.getKey()).append("=").append(entry.getValue());
-                }
+				// Redirect to the URL with only allowed query parameters
+				String redirectUrl = requestURI + "?" + newQueryString;
+				response.sendRedirect(redirectUrl);
+				return false;
+			}
+		}
+		return true;
+	}
 
-                // Redirect to the URL with only allowed query parameters
-                String redirectUrl = requestURI + "?" + newQueryString;
-                response.sendRedirect(redirectUrl);
-                return false;
-            }
-        }
-        return true;
-    }
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) {
+	}
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-    }
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex) {
+	}
 }
