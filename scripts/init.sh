ff7f089f69b1fee8a162da29bd16228f4fdcd5c0
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2023-08-30 22:55:33 +0100

    v tag fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -19,8 +19,15 @@ fi
 # Check for DOCKER_ENABLE_SECURITY and download the appropriate JAR if required
 if [ "$DOCKER_ENABLE_SECURITY" = "true" ] && [ "$VERSION_TAG" != "alpha" ]; then
     if [ ! -f app-security.jar ]; then
-        echo "Downloading from: https://github.com/Frooodle/Stirling-PDF/releases/download/$VERSION_TAG/Stirling-PDF-with-login.jar"
-        curl -L -o app-security.jar https://github.com/Frooodle/Stirling-PDF/releases/download/$VERSION_TAG/Stirling-PDF-with-login.jar
+    	echo "Trying to download from: https://github.com/Frooodle/Stirling-PDF/releases/download/v$VERSION_TAG/Stirling-PDF-with-login.jar"
+        curl -L -o app-security.jar https://github.com/Frooodle/Stirling-PDF/releases/download/v$VERSION_TAG/Stirling-PDF-with-login.jar
+       
+        # If the first download attempt failed, try with the 'v' prefix
+        if [ $? -ne 0 ]; then
+            echo "Trying to download from: https://github.com/Frooodle/Stirling-PDF/releases/download/$VERSION_TAG/Stirling-PDF-with-login.jar"
+        	curl -L -o app-security.jar https://github.com/Frooodle/Stirling-PDF/releases/download/$VERSION_TAG/Stirling-PDF-with-login.jar
+        fi
+
         if [ $? -eq 0 ]; then  # checks if curl was successful
             rm -f app.jar
             ln -s app-security.jar app.jar
