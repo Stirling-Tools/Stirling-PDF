9ab4d9f585766a1c3e0a644dfe42fdcb927140f0
Felix Kaspar <ich@felixkaspar.com>
2023-10-22 16:43:42 +0200

    Reset

--- src/main/java/stirling/software/SPDF/repository/PersistentLoginRepository.java
+++
@@ -1,8 +0,0 @@
-package stirling.software.SPDF.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import stirling.software.SPDF.model.PersistentLogin;
-
-public interface PersistentLoginRepository extends JpaRepository<PersistentLogin, String> {
-}
