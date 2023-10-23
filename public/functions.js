27842a43d2fcbe77967b401ec3e08ca5c5cc28aa
Felix Kaspar <ich@felixkaspar.com>
2023-10-23 14:11:49 +0200

    Organize Pages

--- public/functions.js
+++ public/functions.js
@@ -9,6 +9,7 @@ import { scaleContent as dependantScaleContent} from './functions/scaleContent.j
 import { scalePage as dependantScalePage } from './functions/scalePage.js';
 import { splitPDF as dependantSplitPDF } from './functions/splitPDF.js';
 import { editMetadata as dependantEditMetadata} from "./functions/editMetadata.js";
+import { organizePages as dependantOrganizePages} from "./functions/organizePages.js";
 
 export async function extractPages(snapshot, pagesToExtractArray) {
     return dependantExtractPages(snapshot, pagesToExtractArray, PDFLib);
@@ -40,4 +41,8 @@ export async function splitPDF(snapshot, splitAfterPageArray) {
 
 export async function editMetadata(snapshot, metadata) {
     return dependantEditMetadata(snapshot, metadata, PDFLib);
+}
+
+export async function organizePages(snapshot, operation, customOrderString) {
+    return dependantOrganizePages(snapshot, operation, customOrderString, PDFLib);
 }
