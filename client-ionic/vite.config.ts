88550e13ddaacdb325e2e12e09937b97c9842748
Saud Fatayerji <saudfatayerji@gmail.com>
2023-10-28 00:20:24 +0300

    Added linux release scripts

--- client-ionic/vite.config.ts
+++ client-ionic/vite.config.ts
@@ -1,11 +1,13 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'; // https://ionicframework.com/docs/react/pwa
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({ registerType: 'autoUpdate' }),
     legacy()
   ],
   test: {
