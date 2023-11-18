c060b2a4e0179a6ee203ed12f6f957e80654735a
Saud Fatayerji <saudfatayerji@gmail.com>
2023-11-18 19:56:23 +0300

    Fixed variable name

--- shared-operations/src/functions/removeBlankPages.ts
+++ shared-operations/src/functions/removeBlankPages.ts
@@ -10,11 +10,12 @@ export type RemoveBlankPagesParamsType = {
 }
 export async function removeBlankPages(params: RemoveBlankPagesParamsType) {
     const { file, whiteThreashold } = params;
-    const pageCount = await file.pdfLibDocument;
+    const pdfDoc = await file.pdfLibDocument;
+    const pageCount = pdfDoc.getPageCount();
 
     const emptyPages = await detectEmptyPages(file, whiteThreashold);
     console.debug("Empty Pages: ", emptyPages);
-    const pagesToKeep = invertSelection(emptyPages, pageCount.getPageCount())
+    const pagesToKeep = invertSelection(emptyPages, pageCount)
 
     const newFile = await getPages(file, pagesToKeep);
     newFile.filename += "_removedBlanks"
