26dcc4843e67b12aa8dd625802506c738e23592c
Felix Kaspar <ich@felixkaspar.com>
2023-11-21 22:27:01 +0100

    fixed nested function in workflow validator

--- shared-operations/src/workflow/validateOperations.ts
+++ shared-operations/src/workflow/validateOperations.ts
@@ -1,35 +1,32 @@
 import { Action } from "../../declarations/Action";
 import { getOperatorByName } from "./getOperatorByName";
 
+/** This function validates the "workflow-json" from the API */
 export function validateOperations(actions: Action[]): { valid: boolean, reason?: string} {
-    function validateOperation(actions: Action[]): { valid: boolean; reason?: string; } {
-        for (const action of actions) {
-            if (action.type === "wait" || action.type === "done") {
-                // TODO: Validate these too ):
-                return { valid: true };
+    for (const action of actions) {
+        if (action.type === "wait" || action.type === "done") {
+            // TODO: Validate these too ):
+            return { valid: true };
+        }
+        else {
+            const operator = getOperatorByName(action.type);
+            if(!operator) {
+                return { valid: false, reason: `action.type ${action.type} does not exist` }
             }
-            else {
-                const operator = getOperatorByName(action.type);
-                if(!operator) {
-                    return { valid: false, reason: `action.type ${action.type} does not exist` }
-                }
-                const validationResult = new operator(action).validate();
-    
-                if(!validationResult.valid) {
-                    return validationResult;
-                }
-            }
+            const validationResult = new operator(action).validate();
 
-            if (action.actions) {
-                const validationResult = validateOperation(action.actions);
-
-                if(!validationResult.valid) {
-                    return validationResult;
-                }
+            if(!validationResult.valid) {
+                return validationResult;
             }
         }
-        return { valid: true };
-    }
 
-    return validateOperation(actions);
+        if (action.actions) {
+            const validationResult = validateOperations(action.actions);
+
+            if(!validationResult.valid) {
+                return validationResult;
+            }
+        }
+    }
+    return { valid: true };
 }
