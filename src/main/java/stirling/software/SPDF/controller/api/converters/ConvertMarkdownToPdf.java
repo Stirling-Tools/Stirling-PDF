81d49b722bac2e223851407532083b21dfc600ac
TieStone <416138794@qq.com>
2024-01-10 21:29:43 +0800

    add table support in md2pdf transport

--- src/main/java/stirling/software/SPDF/controller/api/converters/ConvertMarkdownToPdf.java
+++ src/main/java/stirling/software/SPDF/controller/api/converters/ConvertMarkdownToPdf.java
@@ -65,7 +65,6 @@ public class ConvertMarkdownToPdf {
                         .build();
 
         String htmlContent = renderer.render(document);
-        System.out.println(htmlContent);
 
         byte[] pdfBytes =
                 FileToPdf.convertHtmlToPdf(
