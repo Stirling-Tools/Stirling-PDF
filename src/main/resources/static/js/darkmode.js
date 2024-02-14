16efb0f7aa23f4efef5c14918987e0380c0d1741
Ludy87 <Ludy87@users.noreply.github.com>
2024-02-14 10:58:14 +0100

    JS corrections 2

--- src/main/resources/static/js/darkmode.js
+++ src/main/resources/static/js/darkmode.js
@@ -1,5 +1,5 @@
-var toggleCount = 0
-var lastToggleTime = Date.now()
+var toggleCount = 0;
+var lastToggleTime = Date.now();
 
 var elements = {
   lightModeStyles: null,
@@ -11,18 +11,18 @@ var elements = {
   navbar: null,
   navIcons: null,
   navDropdownMenus: null,
-}
+};
 
 function getElements() {
-  elements.lightModeStyles = document.getElementById("light-mode-styles")
-  elements.darkModeStyles = document.getElementById("dark-mode-styles")
-  elements.rainbowModeStyles = document.getElementById("rainbow-mode-styles")
-  elements.darkModeIcon = document.getElementById("dark-mode-icon")
-  elements.searchBar = document.getElementById("searchBar")
-  elements.formControls = document.querySelectorAll(".form-control")
-  elements.navbar = document.querySelectorAll("nav.navbar")
-  elements.navIcons = document.querySelectorAll("nav .icon, .navbar-icon")
-  elements.navDropdownMenus = document.querySelectorAll(".dropdown-menu")
+  elements.lightModeStyles = document.getElementById("light-mode-styles");
+  elements.darkModeStyles = document.getElementById("dark-mode-styles");
+  elements.rainbowModeStyles = document.getElementById("rainbow-mode-styles");
+  elements.darkModeIcon = document.getElementById("dark-mode-icon");
+  elements.searchBar = document.getElementById("searchBar");
+  elements.formControls = document.querySelectorAll(".form-control");
+  elements.navbar = document.querySelectorAll("nav.navbar");
+  elements.navIcons = document.querySelectorAll("nav .icon, .navbar-icon");
+  elements.navDropdownMenus = document.querySelectorAll(".dropdown-menu");
 }
 function setMode(mode) {
   var event = new CustomEvent("modeChanged", { detail: mode });
@@ -48,22 +48,26 @@ function setMode(mode) {
       elements.searchBar.classList.add("dark-mode-search");
     }
     if (elements && elements.formControls) {
-      elements.formControls.forEach(input => input.classList.add("bg-dark", "text-white"));
+      elements.formControls.forEach((input) =>
+        input.classList.add("bg-dark", "text-white"),
+      );
     }
     if (elements && elements.navbar) {
-      elements.navbar.forEach(navElement => {
+      elements.navbar.forEach((navElement) => {
         navElement.classList.remove("navbar-light", "bg-light");
         navElement.classList.add("navbar-dark", "bg-dark");
       });
     }
     if (elements && elements.navDropdownMenus) {
-      elements.navDropdownMenus.forEach(menu => menu.classList.add("dropdown-menu-dark"));
+      elements.navDropdownMenus.forEach((menu) =>
+        menu.classList.add("dropdown-menu-dark"),
+      );
     }
     if (elements && elements.navIcons) {
-      elements.navIcons.forEach(icon => (icon.style.filter = "invert(1)"));
+      elements.navIcons.forEach((icon) => (icon.style.filter = "invert(1)"));
     }
     var tables = document.querySelectorAll(".table");
-    tables.forEach(table => {
+    tables.forEach((table) => {
       table.classList.add("table-dark");
     });
     if (jumbotron) {
@@ -78,22 +82,26 @@ function setMode(mode) {
       elements.searchBar.classList.remove("dark-mode-search");
     }
     if (elements && elements.formControls) {
-      elements.formControls.forEach(input => input.classList.remove("bg-dark", "text-white"));
+      elements.formControls.forEach((input) =>
+        input.classList.remove("bg-dark", "text-white"),
+      );
     }
     if (elements && elements.navbar) {
-      elements.navbar.forEach(navElement => {
+      elements.navbar.forEach((navElement) => {
         navElement.classList.remove("navbar-dark", "bg-dark");
         navElement.classList.add("navbar-light", "bg-light");
       });
     }
     if (elements && elements.navDropdownMenus) {
-      elements.navDropdownMenus.forEach(menu => menu.classList.remove("dropdown-menu-dark"));
+      elements.navDropdownMenus.forEach((menu) =>
+        menu.classList.remove("dropdown-menu-dark"),
+      );
     }
     if (elements && elements.navIcons) {
-      elements.navIcons.forEach(icon => (icon.style.filter = "none"));
+      elements.navIcons.forEach((icon) => (icon.style.filter = "none"));
     }
     var tables = document.querySelectorAll(".table-dark");
-    tables.forEach(table => {
+    tables.forEach((table) => {
       table.classList.remove("table-dark");
     });
     if (jumbotron) {
@@ -108,43 +116,50 @@ function setMode(mode) {
 }
 
 function toggleDarkMode() {
-  var currentTime = Date.now()
+  var currentTime = Date.now();
   if (currentTime - lastToggleTime < 1000) {
-    toggleCount++
+    toggleCount++;
   } else {
-    toggleCount = 1
+    toggleCount = 1;
   }
-  lastToggleTime = currentTime
+  lastToggleTime = currentTime;
 
   if (toggleCount >= 18) {
-    localStorage.setItem("dark-mode", "rainbow")
-    setMode("rainbow")
+    localStorage.setItem("dark-mode", "rainbow");
+    setMode("rainbow");
   } else if (localStorage.getItem("dark-mode") == "on") {
-    localStorage.setItem("dark-mode", "off")
-    setMode("off")
+    localStorage.setItem("dark-mode", "off");
+    setMode("off");
   } else {
-    localStorage.setItem("dark-mode", "on")
-    setMode("on")
+    localStorage.setItem("dark-mode", "on");
+    setMode("on");
   }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  getElements()
+  getElements();
 
-  var currentMode = localStorage.getItem("dark-mode")
-  if (currentMode === "on" || currentMode === "off" || currentMode === "rainbow") {
-    setMode(currentMode)
-  } else if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    setMode("on")
+  var currentMode = localStorage.getItem("dark-mode");
+  if (
+    currentMode === "on" ||
+    currentMode === "off" ||
+    currentMode === "rainbow"
+  ) {
+    setMode(currentMode);
+  } else if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    setMode("on");
   } else {
-    setMode("off")
+    setMode("off");
   }
 
   var darkModeToggle = document.getElementById("dark-mode-toggle");
   if (darkModeToggle !== null) {
-	  darkModeToggle.addEventListener("click", function (event) {
-	    event.preventDefault();
-	    toggleDarkMode();
-	  });
-	}
-})
+    darkModeToggle.addEventListener("click", function (event) {
+      event.preventDefault();
+      toggleDarkMode();
+    });
+  }
+});
--- src/main/resources/static/js/homecard.js
+++ src/main/resources/static/js/homecard.js
@@ -1,78 +1,78 @@
 function filterCards() {
-    var input = document.getElementById('searchBar');
-    var filter = input.value.toUpperCase();
-    var cards = document.querySelectorAll('.feature-card');
+  var input = document.getElementById("searchBar");
+  var filter = input.value.toUpperCase();
+  var cards = document.querySelectorAll(".feature-card");
 
-    for (var i = 0; i < cards.length; i++) {
-        var card = cards[i];
-        var title = card.querySelector('h5.card-title').innerText;
-        var text = card.querySelector('p.card-text').innerText;
+  for (var i = 0; i < cards.length; i++) {
+    var card = cards[i];
+    var title = card.querySelector("h5.card-title").innerText;
+    var text = card.querySelector("p.card-text").innerText;
 
-        // Get the navbar tags associated with the card
-        var navbarItem = document.querySelector(`a.dropdown-item[href="${card.id}"]`);
-        var navbarTags = navbarItem ? navbarItem.getAttribute('data-bs-tags') : '';
+    // Get the navbar tags associated with the card
+    var navbarItem = document.querySelector(
+      `a.dropdown-item[href="${card.id}"]`,
+    );
+    var navbarTags = navbarItem ? navbarItem.getAttribute("data-bs-tags") : "";
 
-        var content = title + ' ' + text + ' ' + navbarTags;
+    var content = title + " " + text + " " + navbarTags;
 
-        if (content.toUpperCase().indexOf(filter) > -1) {
-            card.style.display = "";
-        } else {
-            card.style.display = "none";
-        }
+    if (content.toUpperCase().indexOf(filter) > -1) {
+      card.style.display = "";
+    } else {
+      card.style.display = "none";
     }
+  }
 }
 
-
-
 function toggleFavorite(element) {
-	var img = element.querySelector('img');
-	var card = element.closest('.feature-card');
-	var cardId = card.id;
-	if (img.src.endsWith('star.svg')) {
-		img.src = 'images/star-fill.svg';
-		card.classList.add('favorite');
-		localStorage.setItem(cardId, 'favorite');
-	} else {
-		img.src = 'images/star.svg';
-		card.classList.remove('favorite');
-		localStorage.removeItem(cardId);
-	}
-	reorderCards();
-	updateFavoritesDropdown();
-	filterCards();
+  var img = element.querySelector("img");
+  var card = element.closest(".feature-card");
+  var cardId = card.id;
+  if (img.src.endsWith("star.svg")) {
+    img.src = "images/star-fill.svg";
+    card.classList.add("favorite");
+    localStorage.setItem(cardId, "favorite");
+  } else {
+    img.src = "images/star.svg";
+    card.classList.remove("favorite");
+    localStorage.removeItem(cardId);
+  }
+  reorderCards();
+  updateFavoritesDropdown();
+  filterCards();
 }
 
 function reorderCards() {
-	var container = document.querySelector('.features-container');
-	var cards = Array.from(container.getElementsByClassName('feature-card'));
-	cards.sort(function(a, b) {
-		var aIsFavorite = localStorage.getItem(a.id) === 'favorite';
-		var bIsFavorite = localStorage.getItem(b.id) === 'favorite';
-		if (aIsFavorite && !bIsFavorite) {
-			return -1;
-		}
-		if (!aIsFavorite && bIsFavorite) {
-			return 1;
-		}
-		return 0;
-	});
-	cards.forEach(function(card) {
-		container.appendChild(card);
-	});
+  var container = document.querySelector(".features-container");
+  var cards = Array.from(container.getElementsByClassName("feature-card"));
+  cards.sort(function (a, b) {
+    var aIsFavorite = localStorage.getItem(a.id) === "favorite";
+    var bIsFavorite = localStorage.getItem(b.id) === "favorite";
+    if (aIsFavorite && !bIsFavorite) {
+      return -1;
+    }
+    if (!aIsFavorite && bIsFavorite) {
+      return 1;
+    }
+    return 0;
+  });
+  cards.forEach(function (card) {
+    container.appendChild(card);
+  });
 }
 function initializeCards() {
-	var cards = document.querySelectorAll('.feature-card');
-	cards.forEach(function(card) {
-		var cardId = card.id;
-		var img = card.querySelector('.favorite-icon img');
-		if (localStorage.getItem(cardId) === 'favorite') {
-			img.src = 'images/star-fill.svg';
-			card.classList.add('favorite');
-		}
-	});
-	reorderCards();
-	updateFavoritesDropdown();
-	filterCards();
+  var cards = document.querySelectorAll(".feature-card");
+  cards.forEach(function (card) {
+    var cardId = card.id;
+    var img = card.querySelector(".favorite-icon img");
+    if (localStorage.getItem(cardId) === "favorite") {
+      img.src = "images/star-fill.svg";
+      card.classList.add("favorite");
+    }
+  });
+  reorderCards();
+  updateFavoritesDropdown();
+  filterCards();
 }
 
 window.onload = initializeCards;
