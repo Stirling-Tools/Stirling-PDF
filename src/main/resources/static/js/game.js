655148d8b492d950ab52267af00e4326bd80d657
Ludy87 <Ludy87@users.noreply.github.com>
2024-02-18 21:11:37 +0100

    Update game.js

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -32,7 +32,8 @@ function initializeGame() {
   const BASE_SPAWN_INTERVAL_MS = 1250; // milliseconds before a new enemy spawns
   const LEVEL_INCREASE_FACTOR_MS = 25; // milliseconds to decrease the spawn interval per level
   const MAX_SPAWN_RATE_REDUCTION_MS = 800; // Max milliseconds from the base spawn interval
-
+  
+  
   let keysPressed = {};
   const pdfs = [];
   const projectiles = [];
@@ -41,10 +42,11 @@ function initializeGame() {
   let pdfSpeed = BASE_PDF_SPEED;
   let gameOver = false;
 
+  
   function handleKeys() {
     if (keysPressed["ArrowLeft"]) {
       playerX -= PLAYER_MOVE_SPEED;
-      playerX = Math.max(0, playerX);
+      playerX = Math.max(0, playerX)
     }
     if (keysPressed["ArrowRight"]) {
       playerX += PLAYER_MOVE_SPEED;
@@ -68,9 +70,9 @@ function initializeGame() {
     handleKeys();
   }
   function onKeyUp(event) {
-    keysPressed[event.key] = false;
+	  keysPressed[event.key] = false;
   }
-
+  
   document.removeEventListener("keydown", onKeydown);
   document.removeEventListener("keyup", onKeyUp);
   document.addEventListener("keydown", onKeydown);
@@ -107,7 +109,7 @@ function initializeGame() {
     pdf.classList.add("pdf");
     pdf.style.width = pdfSize + "px";
     pdf.style.height = pdfSize + "px";
-    pdf.style.left = Math.floor(Math.random() * (gameContainer.clientWidth - 2 * pdfSize)) + pdfSize + "px";
+    pdf.style.left = Math.floor(Math.random() * (gameContainer.clientWidth - (2*pdfSize))) + pdfSize + "px";
     pdf.style.top = "0px";
     gameContainer.appendChild(pdf);
     pdfs.push(pdf);
@@ -121,7 +123,7 @@ function initializeGame() {
   function updateGame() {
     if (gameOver || paused) return;
 
-    handleKeys();
+	handleKeys();
     for (let pdfIndex = 0; pdfIndex < pdfs.length; pdfIndex++) {
       const pdf = pdfs[pdfIndex];
       const pdfY = parseFloat(pdf.style.top) + pdfSpeed;
@@ -240,6 +242,8 @@ function initializeGame() {
   }
 
   let spawnPdfTimeout;
+
+  
 
   function spawnPdfInterval() {
     if (gameOver || paused) {
