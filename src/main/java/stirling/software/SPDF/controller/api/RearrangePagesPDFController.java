955c276e1101847eee0adb6b3768aad9db7bfe81
Ludy87 <Ludy87@users.noreply.github.com>
2024-03-04 07:32:26 +0100

    Update RearrangePagesPDFController.java

--- src/main/java/stirling/software/SPDF/controller/api/RearrangePagesPDFController.java
+++ src/main/java/stirling/software/SPDF/controller/api/RearrangePagesPDFController.java
@@ -51,7 +51,7 @@ public class RearrangePagesPDFController {
         String[] pageOrderArr = pagesToDelete.split(",");
 
         List<Integer> pagesToRemove =
-                GeneralUtils.parsePageList(pageOrderArr, document.getNumberOfPages());
+                GeneralUtils.parsePageList(pageOrderArr, document.getNumberOfPages(), true);
 
         Collections.sort(pagesToRemove);
 
@@ -195,7 +195,7 @@ public class RearrangePagesPDFController {
             if (sortType != null && sortType.length() > 0) {
                 newPageOrder = processSortTypes(sortType, totalPages);
             } else {
-                newPageOrder = GeneralUtils.parsePageList(pageOrderArr, totalPages);
+                newPageOrder = GeneralUtils.parsePageList(pageOrderArr, totalPages, true);
             }
             logger.info("newPageOrder = " + newPageOrder);
             logger.info("totalPages = " + totalPages);
