052a1afe3992a59fc16e0dc6f7b04d3194cba03c
Anthony Stirling <77850077+Frooodle@users.noreply.github.com.>
2024-03-08 17:41:28 +0000

    session management fixes and avoid demo user locking

--- src/main/java/stirling/software/SPDF/config/security/CustomAuthenticationFailureHandler.java
+++ src/main/java/stirling/software/SPDF/config/security/CustomAuthenticationFailureHandler.java
@@ -1,6 +1,7 @@
 package stirling.software.SPDF.config.security;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -12,15 +13,19 @@ import org.springframework.stereotype.Component;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import stirling.software.SPDF.model.User;
 
 @Component
 public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Autowired private final LoginAttemptService loginAttemptService;
 
-    @Autowired
-    public CustomAuthenticationFailureHandler(LoginAttemptService loginAttemptService) {
+    @Autowired private final UserService userService; // Inject the UserService
+
+    public CustomAuthenticationFailureHandler(
+            LoginAttemptService loginAttemptService, UserService userService) {
         this.loginAttemptService = loginAttemptService;
+        this.userService = userService;
     }
 
     @Override
@@ -33,17 +38,27 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
         logger.error("Failed login attempt from IP: " + ip);
 
         String username = request.getParameter("username");
-        if (loginAttemptService.loginAttemptCheck(username)) {
-            setDefaultFailureUrl("/login?error=locked");
-
-        } else {
-            if (exception.getClass().isAssignableFrom(BadCredentialsException.class)) {
-                setDefaultFailureUrl("/login?error=badcredentials");
-            } else if (exception.getClass().isAssignableFrom(LockedException.class)) {
+        if (!isDemoUser(username)) {
+            if (loginAttemptService.loginAttemptCheck(username)) {
                 setDefaultFailureUrl("/login?error=locked");
+
+            } else {
+                if (exception.getClass().isAssignableFrom(LockedException.class)) {
+                    setDefaultFailureUrl("/login?error=locked");
+                }
             }
+        }
+        if (exception.getClass().isAssignableFrom(BadCredentialsException.class)) {
+            setDefaultFailureUrl("/login?error=badcredentials");
         }
 
         super.onAuthenticationFailure(request, response, exception);
+    }
+
+    private boolean isDemoUser(String username) {
+        Optional<User> user = userService.findByUsername(username);
+        return user.isPresent()
+                && user.get().getAuthorities().stream()
+                        .anyMatch(authority -> authority.getAuthority().equals("ROLE_DEMO_USER"));
     }
 }
