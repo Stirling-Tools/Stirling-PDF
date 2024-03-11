28c1e6d572cb36287ae333330dfef08a7f6430f2
Anthony Stirling <77850077+Frooodle@users.noreply.github.com.>
2024-03-11 23:06:09 +0000

    compress fixes

--- src/main/java/stirling/software/SPDF/controller/api/pipeline/PipelineController.java
+++ src/main/java/stirling/software/SPDF/controller/api/pipeline/PipelineController.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
