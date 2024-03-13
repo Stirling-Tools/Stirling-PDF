be243836f4a6c8f3df098bd07f5d27f0ba262c51
Ludy <Ludy87@users.noreply.github.com>
2024-03-13 23:09:16 +0100

    Enforcing Username Uniqueness (#906)
    * Enforcing Username Uniqueness
    
    Changes in UserService.java:
    
    Added a new method findByUsername to allow searching for usernames regardless of case sensitivity.
    Added a new method isUsernameValid to validate the username.
    Changes in UserController.java:
    
    Updated the changeUsername method to ensure the new username is valid before changing it.
    Updated the editUser method to ensure the new username is unique and valid.
    Changes in UserRepository.java:
    
    Added a custom JPQL query to search for usernames regardless of case sensitivity.
    Changes in HTML templates (account.html and addUsers.html):
    
    Error messages are displayed if a username is invalid or already exists.
    
    * JPAs auto

--- src/main/java/stirling/software/SPDF/repository/UserRepository.java
+++ src/main/java/stirling/software/SPDF/repository/UserRepository.java
@@ -3,10 +3,14 @@ package stirling.software.SPDF.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import stirling.software.SPDF.model.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsernameIgnoreCase(String username);
+
     Optional<User> findByUsername(String username);
 
     User findByApiKey(String apiKey);
