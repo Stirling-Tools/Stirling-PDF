9d540d6aa21e96900716ceaf54664ac82857e912
Felix Kaspar <ich@felixkaspar.com>
2024-05-27 21:01:24 +0200

    tryping

---
+++ server-node/declarations/ExpressUser.d.ts
@@ -0,0 +1,5 @@
+declare namespace Express {
+    interface User {
+        id?: number;
+    }
+}
--- server-node/src/auth/passport-config.ts
+++ server-node/src/auth/passport-config.ts
@@ -47,3 +47,4 @@ export function initialize(passport: typeof import("passport")) {
         });
     });
 }
+
