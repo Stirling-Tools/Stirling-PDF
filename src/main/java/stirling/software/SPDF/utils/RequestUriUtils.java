233a3e3eaba6a547ccd2cca92f95e77128a5a28a
Ludy87 <Ludy87@users.noreply.github.com>
2024-06-10 21:32:46 +0200

    Bugfix: missing contextPath

--- src/main/java/stirling/software/SPDF/utils/RequestUriUtils.java
+++ src/main/java/stirling/software/SPDF/utils/RequestUriUtils.java
@@ -11,6 +11,20 @@ public class RequestUriUtils {
                 || requestURI.startsWith("/public/")
                 || requestURI.startsWith("/pdfjs/")
                 || requestURI.endsWith(".svg")
+                || requestURI.endsWith(".webmanifest")
                 || requestURI.startsWith("/api/v1/info/status");
+    }
+
+    public static boolean isStaticResource(String contextPath, String requestURI) {
+
+        return requestURI.startsWith(contextPath + "/css/")
+                || requestURI.startsWith(contextPath + "/fonts/")
+                || requestURI.startsWith(contextPath + "/js/")
+                || requestURI.startsWith(contextPath + "/images/")
+                || requestURI.startsWith(contextPath + "/public/")
+                || requestURI.startsWith(contextPath + "/pdfjs/")
+                || requestURI.endsWith(".svg")
+                || requestURI.endsWith(".webmanifest")
+                || requestURI.startsWith(contextPath + "/api/v1/info/status");
     }
 }
