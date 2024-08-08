9277744adcd3ec9b5b48967fb0b56788862561f7
Ping Lin <limphoebe8@gmail.com>
2024-08-08 14:25:58 +0100

    Add comments to PdfImageRemovalController and PdfImageRemovalService.

--- src/main/java/stirling/software/SPDF/controller/api/PdfImageRemovalController.java
+++ src/main/java/stirling/software/SPDF/controller/api/PdfImageRemovalController.java
@@ -16,17 +16,37 @@ import stirling.software.SPDF.model.api.PDFFile;
 import stirling.software.SPDF.service.PdfImageRemovalService;
 import stirling.software.SPDF.utils.WebResponseUtils;
 
+
+/**
+ * Controller class for handling PDF image removal requests.
+ * Provides an endpoint to remove images from a PDF file to reduce its size.
+ */
 @RestController
 @RequestMapping("/api/v1/general")
 public class PdfImageRemovalController {
 
+    // Service for removing images from PDFs
     @Autowired private PdfImageRemovalService pdfImageRemovalService;
 
-    // Constructor injection for PdfImageRemover service
+    /**
+     * Constructor for dependency injection of PdfImageRemovalService.
+     *
+     * @param pdfImageRemovalService The service used for removing images from PDFs.
+     */
     public PdfImageRemovalController(PdfImageRemovalService pdfImageRemovalService) {
         this.pdfImageRemovalService = pdfImageRemovalService;
     }
 
+    /**
+     * Endpoint to remove images from a PDF file.
+     *
+     * This method processes the uploaded PDF file, removes all images, and returns
+     * the modified PDF file with a new name indicating that images were removed.
+     *
+     * @param file The PDF file with images to be removed.
+     * @return ResponseEntity containing the modified PDF file as byte array with appropriate content type and filename.
+     * @throws IOException If an error occurs while processing the PDF file.
+     */
     @PostMapping(consumes = "multipart/form-data", value = "/remove-image-pdf")
     @Operation(
             summary = "Remove images from file to reduce the file size.",
@@ -35,19 +55,28 @@ public class PdfImageRemovalController {
     public ResponseEntity<byte[]> removeImages(@ModelAttribute PDFFile file) throws IOException {
 
         MultipartFile pdf = file.getFileInput();
+
+        // Convert the MultipartFile to a byte array
         byte[] pdfBytes = pdf.getBytes();
+
+        // Load the PDF document from the byte array
         PDDocument document = Loader.loadPDF(pdfBytes);
 
+        // Remove images from the PDF document using the service
         PDDocument modifiedDocument = pdfImageRemovalService.removeImagesFromPdf(document);
 
+        // Create a ByteArrayOutputStream to hold the modified PDF data
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
+        // Save the modified PDF document to the output stream
         modifiedDocument.save(outputStream);
         modifiedDocument.close();
 
+        // Generate a new filename for the modified PDF
         String mergedFileName =
                 pdf.getOriginalFilename().replaceFirst("[.][^.]+$", "") + "_removed_images.pdf";
 
+        // Convert the byte array to a web response and return it
         return WebResponseUtils.bytesToWebResponse(outputStream.toByteArray(), mergedFileName);
     }
 }
