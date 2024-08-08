77fb8be7335c40677a392e41ea1cada01612eb88
diallo <ibrahimasory22@hotmail.com>
2024-08-08 20:06:19 +0000

    test: Update URL in ConvertWebsiteToPdfTest for testing

--- src/test/java/stirling/software/SPDF/controller/api/converters/ConvertWebsiteToPdfTest.java
+++ src/test/java/stirling/software/SPDF/controller/api/converters/ConvertWebsiteToPdfTest.java
@@ -26,7 +26,7 @@ public class ConvertWebsiteToPdfTest {
     @Test
     public void test_exemption_is_thrown_when_url_is_not_reachable() {
 
-        String unreachable_Url = "https://www.googleeee.com";
+        String unreachable_Url = "https://www.googleeeexyz.com";
         // Arrange
         ConvertWebsiteToPDF convertWebsiteToPDF = new ConvertWebsiteToPDF();
         UrlToPdfRequest request = new UrlToPdfRequest();
@@ -38,33 +38,4 @@ public class ConvertWebsiteToPdfTest {
         // Assert
         assertEquals("URL is not reachable, please provide a valid URL.", thrown.getMessage());
     }
-
-    @Test
-    public void test_no_exemption_is_thrown_when_valid_url_format_provided() {
-
-        String valid_format_Url = "https://www.google.com";
-        // Arrange
-        ConvertWebsiteToPDF convertWebsiteToPDF = new ConvertWebsiteToPDF();
-        UrlToPdfRequest request = new UrlToPdfRequest();
-        request.setUrlInput(valid_format_Url);
-        // Act
-        assertDoesNotThrow(() -> {
-            convertWebsiteToPDF.urlToPdf(request);
-        });
-    }
-
-    @Test void test_pdf_bytes_are_returned_when_valid_url_provided() {
-        String valid_format_Url = "https://www.google.com";
-        // Arrange
-        ConvertWebsiteToPDF convertWebsiteToPDF = new ConvertWebsiteToPDF();
-        UrlToPdfRequest request = new UrlToPdfRequest();
-        request.setUrlInput(valid_format_Url);
-        // Act
-        ResponseEntity<byte[]> pdfBytes = assertDoesNotThrow(() -> {
-            return convertWebsiteToPDF.urlToPdf(request);
-        });
-        // Assert
-        assertNotNull(pdfBytes.getBody());
-    }
-
 }
