52b50b696983d7e9a1e213203c9f182208307fad
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2024-08-16 11:00:40 +0100

    Update LibreOfficeListener.java

--- src/main/java/stirling/software/SPDF/LibreOfficeListener.java
+++ src/main/java/stirling/software/SPDF/LibreOfficeListener.java
@@ -5,7 +5,6 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
