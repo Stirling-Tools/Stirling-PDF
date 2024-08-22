3ff4a5e5b908363fbcd5b33f2770648ab6216486
a <a>
2024-08-23 00:05:56 +0100

    cleanups!

--- src/main/java/stirling/software/SPDF/utils/GeneralUtils.java
+++ src/main/java/stirling/software/SPDF/utils/GeneralUtils.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,8 +16,6 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
-import java.net.URL;
-import java.net.HttpURLConnection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -73,12 +74,11 @@ public class GeneralUtils {
         } catch (MalformedURLException e) {
             return false;
         }
-       
-    }   
+    }
 
     public static boolean isURLReachable(String urlStr) {
         try {
-            URL url = new URL(urlStr);
+            URL url = URI.create(urlStr).toURL();
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("HEAD");
             int responseCode = connection.getResponseCode();
@@ -112,16 +112,19 @@ public class GeneralUtils {
         sizeStr = sizeStr.replace(",", ".").replace(" ", "");
         try {
             if (sizeStr.endsWith("KB")) {
-                return (long) (Double.parseDouble(sizeStr.substring(0, sizeStr.length() - 2)) * 1024);
+                return (long)
+                        (Double.parseDouble(sizeStr.substring(0, sizeStr.length() - 2)) * 1024);
             } else if (sizeStr.endsWith("MB")) {
-                return (long) (Double.parseDouble(sizeStr.substring(0, sizeStr.length() - 2))
-                        * 1024
-                        * 1024);
+                return (long)
+                        (Double.parseDouble(sizeStr.substring(0, sizeStr.length() - 2))
+                                * 1024
+                                * 1024);
             } else if (sizeStr.endsWith("GB")) {
-                return (long) (Double.parseDouble(sizeStr.substring(0, sizeStr.length() - 2))
-                        * 1024
-                        * 1024
-                        * 1024);
+                return (long)
+                        (Double.parseDouble(sizeStr.substring(0, sizeStr.length() - 2))
+                                * 1024
+                                * 1024
+                                * 1024);
             } else if (sizeStr.endsWith("B")) {
                 return Long.parseLong(sizeStr.substring(0, sizeStr.length() - 1));
             } else {
@@ -191,8 +194,7 @@ public class GeneralUtils {
 
             // Check if the result is null or not within bounds
             if (result == null || result <= 0 || result.intValue() > maxValue) {
-                if (n != 0)
-                    break;
+                if (n != 0) break;
             } else {
                 results.add(result.intValue());
             }
