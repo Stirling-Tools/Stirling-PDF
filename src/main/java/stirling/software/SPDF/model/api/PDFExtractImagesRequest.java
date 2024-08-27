ab45fcbfc0c8aa631fead1deed31576ffa093e7c
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2024-08-27 11:46:18 +0200

    Extract images enhancements (#1757)
    * fix
    
    * extarct images
    
    * langs
    
    * logging
    
    * cuke fix
    
    ---------
    
    Co-authored-by: a <a>

---
+++ src/main/java/stirling/software/SPDF/model/api/PDFExtractImagesRequest.java
@@ -0,0 +1,16 @@
+package stirling.software.SPDF.model.api;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PDFExtractImagesRequest extends PDFWithImageFormatRequest {
+
+    @Schema(
+            description =
+                    "Boolean to enable/disable the saving of duplicate images, true to enable duplicates")
+    private boolean allowDuplicates;
+}
--- src/main/java/stirling/software/SPDF/utils/ImageProcessingUtils.java
+++ src/main/java/stirling/software/SPDF/utils/ImageProcessingUtils.java
@@ -1,6 +1,10 @@
 package stirling.software.SPDF.utils;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.DataBufferInt;
+import java.nio.ByteBuffer;
 
 public class ImageProcessingUtils {
 
@@ -28,5 +32,31 @@ public class ImageProcessingUtils {
                 break;
         }
         return convertedImage;
+    }
+
+    public static byte[] getImageData(BufferedImage image) {
+        DataBuffer dataBuffer = image.getRaster().getDataBuffer();
+        if (dataBuffer instanceof DataBufferByte) {
+            return ((DataBufferByte) dataBuffer).getData();
+        } else if (dataBuffer instanceof DataBufferInt) {
+            int[] intData = ((DataBufferInt) dataBuffer).getData();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(intData.length * 4);
+            byteBuffer.asIntBuffer().put(intData);
+            return byteBuffer.array();
+        } else {
+            int width = image.getWidth();
+            int height = image.getHeight();
+            byte[] data = new byte[width * height * 3];
+            int index = 0;
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    int rgb = image.getRGB(x, y);
+                    data[index++] = (byte) ((rgb >> 16) & 0xFF); // Red
+                    data[index++] = (byte) ((rgb >> 8) & 0xFF); // Green
+                    data[index++] = (byte) (rgb & 0xFF); // Blue
+                }
+            }
+            return data;
+        }
     }
 }
