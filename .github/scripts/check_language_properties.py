11096491e5012374febec0b3c06b8b1a7b21fc0a
Ludy87 <Ludy87@users.noreply.github.com>
2024-08-31 22:21:24 +0200

    Fix: wrong selected repository; disable Push - missing permission

--- .github/scripts/check_language_properties.py
+++ .github/scripts/check_language_properties.py
@@ -165,6 +165,7 @@ def check_for_differences(reference_file, file_list, branch):
                 report.append(
                     f"  - **Issue:** Too many lines! Check your translation files! Details: {reference_line_count} (reference) vs {current_line_count} (current)."
                 )
+            # update_missing_keys(reference_file, [file_path], branch + "/")
         else:
             report.append("- **Test 1 Status:** ✅ Passed")
 
@@ -200,16 +201,19 @@ def check_for_differences(reference_file, file_list, branch):
                 report.append(
                     f"  - **Issue:** There are keys in ***{basename_reference_file}*** `{extra_keys_str}` that are not present in ***{basename_current_file}***!"
                 )
+            # update_missing_keys(reference_file, [file_path], branch + "/")
         else:
             report.append("- **Test 2 Status:** ✅ Passed")
-        if has_differences:
-            report.append("")
-            report.append(f"#### 🚧 ***{basename_current_file}*** will be corrected...")
-            report.append("")
+        # if has_differences:
+        #     report.append("")
+        #     report.append(f"#### 🚧 ***{basename_current_file}*** will be corrected...")
+        report.append("")
         report.append("---")
         report.append("")
-    update_file_list = glob.glob(branch + "/src/**/messages_*.properties", recursive=True)
-    update_missing_keys(reference_file, update_file_list)
+    # update_file_list = glob.glob(branch + "/src/**/messages_*.properties", recursive=True)
+    # update_missing_keys(reference_file, update_file_list)
+    # report.append("---")
+    # report.append("")
     if has_differences:
         report.append("## ❌ Overall Check Status: **_Failed_**")
     else:
