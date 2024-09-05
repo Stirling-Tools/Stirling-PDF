39ca4df41fc30c6d514bb539ac53898a18c90bd2
creator1999 <38176970+creator1999@users.noreply.github.com>
2024-09-05 22:24:38 +0530

    Added functionality to set font size and font type in both frontend and backend. (#1783)
    * Added variables
    
    * Added functionality to add font size and font type in both frontend and backend
    
    * new changes suggested has been added
    
    ---------
    
    Co-authored-by: Anthony Stirling <77850077+Frooodle@users.noreply.github.com>

--- src/main/java/stirling/software/SPDF/repository/UserRepository.java
+++ src/main/java/stirling/software/SPDF/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package stirling.software.SPDF.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import stirling.software.SPDF.model.User;
 
-import java.util.Optional;
+import stirling.software.SPDF.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
