f98344818d3e14c883b7b346a605910b0e58485e
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2024-09-06 16:56:55 +0200

    Footer link to Stirlingpdf.com (#1827)
    * fix
    
    * remove donate
    
    * Footer to have link to website
    
    ---------
    
    Co-authored-by: a <a>

--- src/main/java/stirling/software/SPDF/repository/UserRepository.java
+++ src/main/java/stirling/software/SPDF/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import stirling.software.SPDF.model.User;
@@ -13,7 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsernameIgnoreCase(String username);
 
     @Query("FROM User u LEFT JOIN FETCH u.settings where upper(u.username) = upper(:username)")
-    Optional<User> findByUsernameIgnoreCaseWithSettings(String username);
+    Optional<User> findByUsernameIgnoreCaseWithSettings(@Param("username") String username);
+
 
     Optional<User> findByUsername(String username);
 
