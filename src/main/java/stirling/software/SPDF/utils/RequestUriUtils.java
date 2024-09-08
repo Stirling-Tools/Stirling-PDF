ce5571a3d4a101505b05fe965cf569b0e1fc48aa
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2024-09-08 21:42:22 +0100

    Update RequestUriUtils.java

--- src/main/java/stirling/software/SPDF/utils/RequestUriUtils.java
+++ src/main/java/stirling/software/SPDF/utils/RequestUriUtils.java
@@ -4,16 +4,7 @@ public class RequestUriUtils {
 
     public static boolean isStaticResource(String requestURI) {
 
-        return requestURI.startsWith("/css/")
-                || requestURI.startsWith("/fonts/")
-                || requestURI.startsWith("/js/")
-                || requestURI.startsWith("/images/")
-                || requestURI.startsWith("/public/")
-                || requestURI.startsWith("/pdfjs/")
-                || requestURI.startsWith("/pdfjs-legacy/")
-                || requestURI.endsWith(".svg")
-                || requestURI.endsWith(".webmanifest")
-                || requestURI.startsWith("/api/v1/info/status");
+        return isStaticResource(requestURI, "");
     }
 
     public static boolean isStaticResource(String contextPath, String requestURI) {
@@ -24,7 +15,10 @@ public class RequestUriUtils {
                 || requestURI.startsWith(contextPath + "/images/")
                 || requestURI.startsWith(contextPath + "/public/")
                 || requestURI.startsWith(contextPath + "/pdfjs/")
+                || requestURI.startsWith(contextPath + "/login")
                 || requestURI.endsWith(".svg")
+                || requestURI.endsWith(".png")
+                || requestURI.endsWith(".ico")
                 || requestURI.endsWith(".webmanifest")
                 || requestURI.startsWith(contextPath + "/api/v1/info/status");
     }
