21c667e3923bb60d222de28f2c93b5d1a23b897f
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-12-02 06:20:05 +0200

    Add support for listing and removing selected files and their file icons
    - Selected files are listed below the file chooser in a selected files container.
    - Users can now remove uploaded/selected files.
    - Hide selected files container/box unless there are files selected/uploaded.
    - Add separate overlay for each drag & drop area.
    
    ## FAQ:
    - Why did you assign a unique id to each file? isn't the filename enough?
    = Because a user might upload multiple files with the same name, if the user wanted to remove one of them, how would we differentiate between them? we won't be able to unless we assign an identifier, you might argue "we remove based on the filename and size", then what if the user uploaded the same file more than once (intentionally), then we would accidentally remove all the files even though that is not what the user wanted, so going with unique ID approach would prevent this issue/problem from occurring in the first place.

--- src/main/resources/static/js/fileInput.js
+++ src/main/resources/static/js/fileInput.js
@@ -1,3 +1,6 @@
+import FileIconFactory from "./file-icon-factory.js";
+import FileUtils from "./file-utils.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".custom-file-chooser").forEach(setupFileInput);
 });
@@ -6,6 +9,9 @@ function setupFileInput(chooser) {
   const elementId = chooser.getAttribute("data-bs-element-id");
   const filesSelected = chooser.getAttribute("data-bs-files-selected");
   const pdfPrompt = chooser.getAttribute("data-bs-pdf-prompt");
+  const inputContainerId = chooser.getAttribute('data-bs-element-container-id');
+
+  let inputContainer = document.getElementById(inputContainerId);
 
   let allFiles = [];
   let overlay;
@@ -15,7 +21,7 @@ function setupFileInput(chooser) {
     dragCounter++;
     if (!overlay) {
       // Show overlay by removing display: none from pseudo elements (::before and ::after)
-      chooser.style.setProperty('--overlay-display', "''");
+      inputContainer.style.setProperty('--overlay-display', "''");
       overlay = true;
     }
   };
@@ -29,14 +35,14 @@ function setupFileInput(chooser) {
 
   function hideOverlay() {
     if (!overlay) return;
-    chooser.style.setProperty('--overlay-display', 'none');
+    inputContainer.style.setProperty('--overlay-display', 'none');
     overlay = false;
   }
 
   const dropListener = function (e) {
     e.preventDefault();
     // Drag and Drop shall only affect the target file chooser
-    if (e.target !== chooser) {
+    if (e.target !== inputContainer) {
       hideOverlay();
       dragCounter = 0;
       return;
@@ -47,7 +53,7 @@ function setupFileInput(chooser) {
 
     const fileInput = document.getElementById(elementId);
     if (fileInput?.hasAttribute("multiple")) {
-      files.forEach(file => allFiles.push(file));
+      pushFileListTo(files, allFiles);
     } else if (fileInput) {
       allFiles = [files[0]];
     }
@@ -63,6 +69,12 @@ function setupFileInput(chooser) {
 
     fileInput.dispatchEvent(new CustomEvent("change", { bubbles: true, detail: {source: 'drag-drop'} }));
   };
+
+  function pushFileListTo(fileList, container) {
+    for (let file of fileList) {
+      container.push(file);
+    }
+  }
 
   ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
     document.body.addEventListener(eventName, preventDefaults, false);
@@ -80,16 +92,20 @@ function setupFileInput(chooser) {
   $("#" + elementId).on("change", function (e) {
     let element = e.target;
     const isDragAndDrop = e.detail?.source == 'drag-drop';
-    
+
     if (element instanceof HTMLInputElement && element.hasAttribute("multiple")) {
       allFiles = isDragAndDrop ? allFiles : [... allFiles, ... element.files];
     } else {
       allFiles = Array.from(isDragAndDrop ? allFiles : [element.files[0]]);
     }
 
+    allFiles = allFiles.map(file => {
+      if (!file.uniqueId) file.uniqueId = crypto.randomUUID();
+      return file;
+    });
+
     if (!isDragAndDrop) {
-        let dataTransfer = new DataTransfer();
-        allFiles.forEach(file => dataTransfer.items.add(file));
+        let dataTransfer = toDataTransfer(allFiles);
         element.files = dataTransfer.files;
     }
 
@@ -97,25 +113,100 @@ function setupFileInput(chooser) {
     this.dispatchEvent(new CustomEvent("file-input-change", { bubbles: true }));
 });
 
+  function toDataTransfer(files) {
+    let dataTransfer = new DataTransfer();
+    files.forEach(file => dataTransfer.items.add(file));
+    return dataTransfer;
+  }
+
   function handleFileInputChange(inputElement) {
     const files = allFiles;
-    const fileNames = files.map((f) => f.name);
-    const selectedFilesContainer = $(inputElement).siblings(".selected-files");
+    showOrHideSelectedFilesContainer(files);
+
+    const filesInfo = files.map((f) => ({name: f.name, size: f.size, uniqueId: f.uniqueId}));
+
+    const selectedFilesContainer = $(inputContainer).siblings(".selected-files");
     selectedFilesContainer.empty();
-    fileNames.forEach((fileName) => {
-      selectedFilesContainer.append("<div>" + fileName + "</div>");
+    filesInfo.forEach((info) => {
+      let fileContainerClasses = 'small-file-container d-flex flex-column justify-content-center align-items-center';
+
+      let fileContainer = document.createElement('div');
+      $(fileContainer).addClass(fileContainerClasses);
+      $(fileContainer).attr('id', info.uniqueId);
+
+      let fileIconContainer = createFileIconContainer(info);
+
+      let fileInfoContainer = createFileInfoContainer(info);
+
+      let removeBtn = document.createElement('div');
+      removeBtn.classList.add('remove-file');
+
+      let removeBtnIconHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#C02223"><path d="m339-288 141-141 141 141 51-51-141-141 141-141-51-51-141 141-141-141-51 51 141 141-141 141 51 51ZM480-96q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Z"/></svg>`;
+      $(removeBtn).append(removeBtnIconHTML);
+      $(removeBtn).attr('data-file-id', info.uniqueId).click(removeFileListener);
+
+      $(fileContainer).append(fileIconContainer);
+      $(fileContainer).append(fileInfoContainer);
+      $(fileContainer).append(removeBtn);
+
+      selectedFilesContainer.append(fileContainer);
     });
-    if (fileNames.length === 1) {
-      $(inputElement).siblings(".custom-file-label").addClass("selected").html(fileNames[0]);
-    } else if (fileNames.length > 1) {
-      $(inputElement)
-        .siblings(".custom-file-label")
-        .addClass("selected")
-        .html(fileNames.length + " " + filesSelected);
-    } else {
-      $(inputElement).siblings(".custom-file-label").addClass("selected").html(pdfPrompt);
-    }
+
+    showOrHideSelectedFilesContainer(filesInfo);
   }
+
+  function showOrHideSelectedFilesContainer(files) {
+    if (files && files.length > 0)
+      chooser.style.setProperty('--selected-files-display', 'flex');
+    else
+    chooser.style.setProperty('--selected-files-display', 'none');
+  }
+
+  function removeFileListener(e) {
+    const fileId = (e.target).getAttribute('data-file-id');
+
+    let fileContainer = document.getElementById(fileId);
+    fileContainer.remove();
+
+    allFiles = allFiles.filter(v => v.uniqueId != fileId);
+    let dataTransfer = toDataTransfer(allFiles);
+
+    let inputElement = document.getElementById(elementId);
+    inputElement.files = dataTransfer.files;
+
+    showOrHideSelectedFilesContainer(allFiles);
+
+    this.dispatchEvent(new CustomEvent("file-input-change", { bubbles: true }));
+  }
+
+  function createFileIconContainer(info) {
+    let fileIconContainer = document.createElement('div');
+    fileIconContainer.classList.add('file-icon');
+
+    // Add icon based on the extension
+    let fileExtension = FileUtils.extractFileExtension(info.name);
+    let fileIcon = FileIconFactory.createFileIcon(fileExtension);
+
+    $(fileIconContainer).append(fileIcon);
+    return fileIconContainer;
+  }
+
+  function createFileInfoContainer(info) {
+    let fileInfoContainer = document.createElement("div");
+    let fileInfoContainerClasses = 'file-info d-flex flex-column align-items-center justify-content-center';
+
+    $(fileInfoContainer).addClass(fileInfoContainerClasses);
+
+    $(fileInfoContainer).append(
+      `<div title="${info.name}">${info.name}</div>`
+    );
+    let fileSizeWithUnits = FileUtils.transformFileSize(info.size);
+    $(fileInfoContainer).append(
+      `<div title="${info.size}">${fileSizeWithUnits}</div>`
+    );
+    return fileInfoContainer;
+  }
+
   //Listen for event of file being removed and the filter it out of the allFiles array
   document.addEventListener("fileRemoved", function (e) {
     const fileName = e.detail;
