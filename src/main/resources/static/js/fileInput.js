f00c29959ca5888fc39bfc7efb73ec56b958cbb6
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-12-02 07:01:11 +0200

    Rename remove-file css class to remove-selected-file
    - Rename remove-file css class to remove-selected-file to avoid css conflict with remove-file in merge.css

--- src/main/resources/static/js/fileInput.js
+++ src/main/resources/static/js/fileInput.js
@@ -139,7 +139,7 @@ function setupFileInput(chooser) {
       let fileInfoContainer = createFileInfoContainer(info);
 
       let removeBtn = document.createElement('div');
-      removeBtn.classList.add('remove-file');
+      removeBtn.classList.add('remove-selected-file');
 
       let removeBtnIconHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#C02223"><path d="m339-288 141-141 141 141 51-51-141-141 141-141-51-51-141 141-141-141-51 51 141 141-141 141 51 51ZM480-96q-79 0-149-30t-122.5-82.5Q156-261 126-331T96-480q0-80 30-149.5t82.5-122Q261-804 331-834t149-30q80 0 149.5 30t122 82.5Q804-699 834-629.5T864-480q0 79-30 149t-82.5 122.5Q699-156 629.5-126T480-96Z"/></svg>`;
       $(removeBtn).append(removeBtnIconHTML);
