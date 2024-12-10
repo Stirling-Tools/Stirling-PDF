46fea6a20101ae7300a14bb9585c720b8ae767f5
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-12-10 22:56:04 +0200

    Remove an unused import

--- src/main/java/stirling/software/SPDF/controller/api/security/RedactController.java
+++ src/main/java/stirling/software/SPDF/controller/api/security/RedactController.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
 
-import org.apache.fontbox.util.BoundingBox;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
