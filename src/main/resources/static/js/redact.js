09e8a84d672f47be99bd21c81f879cb1e6a15c3b
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-12-18 23:28:48 +0200

    Bring back nav bar

--- src/main/resources/static/js/redact.js
+++ src/main/resources/static/js/redact.js
@@ -28,7 +28,6 @@ function removePDFJSButtons() {
 }
 
 function hideInitialPage() {
-  document.getElementsByTagName("nav")[0].classList.add("d-none");
   let redactionsFormContainer = document.getElementById(
     "redactionFormContainer"
   );
