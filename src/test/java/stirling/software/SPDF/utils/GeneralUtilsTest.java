cb135240c9d18aef99bad6a3a51fc36bdffac78b
Omar Ahmed Hassan <98468609+omar-ahmed42@users.noreply.github.com>
2025-01-02 16:48:20 +0200

    Enhancement: Enhance NFunction evaluation and support advanced NFunctions (#2577)
    # Description
    
    Enhance NFunction sanitization and support advanced functions:
    - Start page counting from 1 rather than 0 as PDFs are one based from
    the user's perspective, thus functions results would be affected by
    starting with "0" rather than "1".
    - Ignore out of bound results rather than stopping iterations to work
    with functions such as (n - 4) when page count is 10 as we would get
    positive values when n > 4.
    - Remove spaces to support expressions such as 2n + 1 rather just 2n+1.
    - Support advanced functions as follows:
    - Support expressions such as follows 5(n-1), n(n-1), expressions
    followed by opening rounded without '*' operator.
    - Support expressions such as follows (n-1)5, (n-1)n, expressions
    preceded closing rounded without '*' operator.
    - Support consecutive "n" expressions, examples: nnn, 2nn, nn*3, nnnn.
    
    Closes #(issue_number)
    
    ## Checklist
    
    - [x] I have read the [Contribution
    Guidelines](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/CONTRIBUTING.md)
    - [x] I have performed a self-review of my own code
    - [ ] I have attached images of the change if it is UI based
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] If my code has heavily changed functionality I have updated
    relevant docs on [Stirling-PDFs doc
    repo](https://github.com/Stirling-Tools/Stirling-Tools.github.io/blob/main/docs/)
    - [x] My changes generate no new warnings
    - [ ] I have read the section [Add New Translation
    Tags](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/HowToAddNewLanguage.md#add-new-translation-tags)
    (for new translation tags only)

--- src/test/java/stirling/software/SPDF/utils/GeneralUtilsTest.java
+++ src/test/java/stirling/software/SPDF/utils/GeneralUtilsTest.java
@@ -52,14 +52,68 @@ public class GeneralUtilsTest {
     @Test
     void nFuncAdvanced3() {
         List<Integer> result = GeneralUtils.parsePageList(new String[]{"4n+1"}, 9, true);
-        assertEquals(List.of(1, 5, 9), result, "'All' keyword should return all pages.");
+        assertEquals(List.of(5, 9), result, "'All' keyword should return all pages.");
+    }
+
+    @Test
+    void nFunc_spaces() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"n + 1"}, 9, true);
+        assertEquals(List.of(2, 3, 4, 5, 6, 7, 8, 9), result);
+    }
+
+    @Test
+    void nFunc_consecutive_Ns_nnn() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"nnn"}, 9, true);
+        assertEquals(List.of(1, 8), result);
+    }
+
+    @Test
+    void nFunc_consecutive_Ns_nn() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"nn"}, 9, true);
+        assertEquals(List.of(1, 4, 9), result);
+    }
+
+    @Test
+    void nFunc_opening_closing_round_brackets() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"(n-1)(n-2)"}, 9, true);
+        assertEquals(List.of(2, 6), result);
+    }
+
+    @Test
+    void nFunc_opening_round_brackets() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"2(n-1)"}, 9, true);
+        assertEquals(List.of(2, 4, 6, 8), result);
+    }
+
+    @Test
+    void nFunc_opening_round_brackets_n() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"n(n-1)"}, 9, true);
+        assertEquals(List.of(2, 6), result);
+    }
+
+    @Test
+    void nFunc_closing_round_brackets() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"(n-1)2"}, 9, true);
+        assertEquals(List.of(2, 4, 6, 8), result);
+    }
+
+    @Test
+    void nFunc_closing_round_brackets_n() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"(n-1)n"}, 9, true);
+        assertEquals(List.of(2, 6), result);
+    }
+
+    @Test
+    void nFunc_function_surrounded_with_brackets() {
+        List<Integer> result = GeneralUtils.parsePageList(new String[]{"(n-1)"}, 9, true);
+        assertEquals(List.of(1, 2, 3, 4, 5, 6, 7, 8), result);
     }
 
 
     @Test
     void nFuncAdvanced4() {
         List<Integer> result = GeneralUtils.parsePageList(new String[]{"3+2n"}, 9, true);
-        assertEquals(List.of(3, 5, 7, 9), result, "'All' keyword should return all pages.");
+        assertEquals(List.of(5, 7, 9), result, "'All' keyword should return all pages.");
     }
 
     @Test
@@ -79,7 +133,6 @@ public class GeneralUtilsTest {
         List<Integer> result = GeneralUtils.parsePageList(new String[]{"1-3"}, 5, true);
         assertEquals(List.of(1, 2, 3), result, "Range should be parsed correctly.");
     }
-
 
     @Test
     void testParsePageListWithRangeZeroBaseOutput() {
