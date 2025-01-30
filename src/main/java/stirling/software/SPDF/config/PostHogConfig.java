94ea723326aaffe44619b122be086f49fbd2c08f
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2025-01-30 15:13:42 +0000

    Posthog to log to log file (#2813)
    # Description of Changes
    
    Please provide a summary of the changes, including:
    
    - What was changed
    - Why the change was made
    - Any challenges encountered
    
    Closes #(issue_number)
    
    ---
    
    ## Checklist
    
    ### General
    
    - [ ] I have read the [Contribution
    Guidelines](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/CONTRIBUTING.md)
    - [ ] I have read the [Stirling-PDF Developer
    Guide](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/DeveloperGuide.md)
    (if applicable)
    - [ ] I have read the [How to add new languages to
    Stirling-PDF](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/HowToAddNewLanguage.md)
    (if applicable)
    - [ ] I have performed a self-review of my own code
    - [ ] My changes generate no new warnings
    
    ### Documentation
    
    - [ ] I have updated relevant docs on [Stirling-PDF's doc
    repo](https://github.com/Stirling-Tools/Stirling-Tools.github.io/blob/main/docs/)
    (if functionality has heavily changed)
    - [ ] I have read the section [Add New Translation
    Tags](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/HowToAddNewLanguage.md#add-new-translation-tags)
    (for new translation tags only)
    
    ### UI Changes (if applicable)
    
    - [ ] Screenshots or videos demonstrating the UI changes are attached
    (e.g., as comments or direct attachments in the PR)
    
    ### Testing (if applicable)
    
    - [ ] I have tested my changes locally. Refer to the [Testing
    Guide](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/DeveloperGuide.md#6-testing)
    for more details.
    
    ---------
    
    Co-authored-by: a <a>

--- src/main/java/stirling/software/SPDF/config/PostHogConfig.java
+++ src/main/java/stirling/software/SPDF/config/PostHogConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import com.posthog.java.PostHog;
 
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
+@Slf4j
 public class PostHogConfig {
 
     @Value("${posthog.api.key}")
@@ -21,7 +23,11 @@ public class PostHogConfig {
 
     @Bean
     public PostHog postHogClient() {
-        postHogClient = new PostHog.Builder(posthogApiKey).host(posthogHost).build();
+        postHogClient =
+                new PostHog.Builder(posthogApiKey)
+                        .host(posthogHost)
+                        .logger(new PostHogLoggerImpl())
+                        .build();
         return postHogClient;
     }
 
