ba682ddd54976bc2f0167fdccb6c2494eba1230d
Ludy87 <Ludy87@users.noreply.github.com>
2025-02-23 20:55:54 +0100

    Update AppUpdateService.java

--- src/main/java/stirling/software/SPDF/config/AppUpdateService.java
+++ src/main/java/stirling/software/SPDF/config/AppUpdateService.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
 
 import stirling.software.SPDF.config.interfaces.ShowAdminInterface;
 import stirling.software.SPDF.model.ApplicationProperties;
 
 @Configuration
-@Service
 class AppUpdateService {
 
     private final ApplicationProperties applicationProperties;
