c61b748ffd94d9d138e73516bc18228101e9072b
Ludy87 <Ludy87@users.noreply.github.com>
2025-03-09 19:52:58 +0000

    Improve Git initialization script to check username and email and make sure they are set before applying local configurations.

--- .devcontainer/git-init.sh
+++ .devcontainer/git-init.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-# .devcontainer/git-init.sh
 
-echo "git config --global --get user.name"
-git config --global --get user.name
+GIT_USER=$(git config --get user.name)
+GIT_EMAIL=$(git config --get user.email)
 
-echo "git config --get user.name"
-git config --get user.name
-
-# Falls die globale Konfiguration verfügbar ist, diese auslesen; sonst Default-Werte setzen
-GIT_USER=$(git config --get user.name || echo "Dein Name")
-GIT_EMAIL=$(git config --get user.email || echo "deine.email@example.com")
+# Exit if GIT_USER or GIT_EMAIL is empty
+if [ -z "$GIT_USER" ] || [ -z "$GIT_EMAIL" ]; then
+  echo "GIT_USER or GIT_EMAIL is not set. Exiting."
+  exit 1
+fi
 
 git config --local user.name "$GIT_USER"
 git config --local user.email "$GIT_EMAIL"
 
-git config --local --add safe.directory /workspace
+# This directory should contain custom Git hooks for the repository
+# Set the path for Git hooks to /workspace/hooks
 git config --local core.hooksPath /workspace/hooks
+# Set the safe directory to the workspace path
+git config --local --add safe.directory /workspace
