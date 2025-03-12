685faef34f3f1aab4a57d884c5bb5fb151ed4700
Ludy87 <Ludy87@users.noreply.github.com>
2025-03-12 13:33:58 +0100

    Update RotationControllerTest.java

--- src/test/java/stirling/software/SPDF/controller/api/RotationControllerTest.java
+++ src/test/java/stirling/software/SPDF/controller/api/RotationControllerTest.java
@@ -1,36 +1,31 @@
 package stirling.software.SPDF.controller.api;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-
 import org.apache.pdfbox.pdmodel.PDPage;
 import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDPageTree;
-import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import stirling.software.SPDF.service.CustomPDDocumentFactory;
 import stirling.software.SPDF.model.api.general.RotatePDFRequest;
 
+@ExtendWith(MockitoExtension.class)
 public class RotationControllerTest {
 
     @Mock private CustomPDDocumentFactory pdfDocumentFactory;
 
     @InjectMocks private RotationController rotationController;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     public void testRotatePDF() throws IOException {
