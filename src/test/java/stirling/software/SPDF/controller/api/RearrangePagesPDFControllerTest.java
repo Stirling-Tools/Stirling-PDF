cbae0640c973316e594e086fc01db334f443106e
Ludy87 <Ludy87@users.noreply.github.com>
2025-03-13 08:31:31 +0100

    Update RearrangePagesPDFControllerTest.java

--- src/test/java/stirling/software/SPDF/controller/api/RearrangePagesPDFControllerTest.java
+++ src/test/java/stirling/software/SPDF/controller/api/RearrangePagesPDFControllerTest.java
@@ -1,6 +1,5 @@
 package stirling.software.SPDF.controller.api;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
