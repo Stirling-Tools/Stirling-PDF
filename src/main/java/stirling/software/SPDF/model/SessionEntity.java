813897175c8ae55ce8100970fbd4901cfd906877
Ludy87 <Ludy87@users.noreply.github.com>
2025-03-27 13:10:18 +0100

    Update SessionEntity.java

--- src/main/java/stirling/software/SPDF/model/SessionEntity.java
+++ src/main/java/stirling/software/SPDF/model/SessionEntity.java
@@ -9,15 +9,34 @@ import jakarta.persistence.Table;
 
 import lombok.Data;
 
+import stirling.software.SPDF.config.interfaces.SessionsModelInterface;
+
 @Entity
 @Data
 @Table(name = "sessions")
-public class SessionEntity implements Serializable {
+public class SessionEntity implements Serializable, SessionsModelInterface {
     @Id private String sessionId;
-
     private String principalName;
-
     private Date lastRequest;
-
     private boolean expired;
+
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    @Override
+    public String getPrincipalName() {
+        return principalName;
+    }
+
+    @Override
+    public Date getLastRequest() {
+        return lastRequest;
+    }
+
+    @Override
+    public boolean isExpired() {
+        return expired;
+    }
 }
