72c908065718fd33e349a30416df915f5190858e
Ludy87 <Ludy87@users.noreply.github.com>
2025-04-20 12:19:11 +0200

    Update UserUtils.java

--- src/main/java/stirling/software/SPDF/config/security/UserUtils.java
+++ src/main/java/stirling/software/SPDF/config/security/UserUtils.java
@@ -9,6 +9,8 @@ public class UserUtils {
     public static String getUsernameFromPrincipal(Object principal) {
         if (principal instanceof UserDetails detailsUser) {
             return detailsUser.getUsername();
+        } else if (principal instanceof stirling.software.SPDF.model.User domainUser) {
+            return domainUser.getUsername();
         } else if (principal instanceof OAuth2User oAuth2User) {
             return oAuth2User.getName();
         } else if (principal instanceof CustomSaml2AuthenticatedPrincipal saml2User) {
