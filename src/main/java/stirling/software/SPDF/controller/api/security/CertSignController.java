c66dc984c0221544346971e96cbc28f374fb2529
Ludy87 <Ludy87@users.noreply.github.com>
2025-05-02 19:29:40 +0200

    cert-sign

--- src/main/java/stirling/software/SPDF/controller/api/security/CertSignController.java
+++ src/main/java/stirling/software/SPDF/controller/api/security/CertSignController.java
@@ -1,6 +1,7 @@
 package stirling.software.SPDF.controller.api.security;
 
 import java.awt.*;
+import java.beans.PropertyEditorSupport;
 import java.io.*;
 import java.nio.file.Files;
 import java.security.*;
@@ -53,7 +54,10 @@ import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
 import org.bouncycastle.pkcs.PKCSException;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -82,6 +86,18 @@ public class CertSignController {
         Security.addProvider(new BouncyCastleProvider());
     }
 
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(
+                MultipartFile.class,
+                new PropertyEditorSupport() {
+                    @Override
+                    public void setAsText(String text) throws IllegalArgumentException {
+                        setValue(null);
+                    }
+                });
+    }
+
     private final CustomPDFDocumentFactory pdfDocumentFactory;
 
     private static void sign(
@@ -103,8 +119,7 @@ public class CertSignController {
             signature.setLocation(location);
             signature.setReason(reason);
             signature.setSignDate(Calendar.getInstance());
-
-            if (showSignature) {
+            if (Boolean.TRUE.equals(showSignature)) {
                 SignatureOptions signatureOptions = new SignatureOptions();
                 signatureOptions.setVisualSignature(
                         instance.createVisibleSignature(doc, signature, pageNumber, showLogo));
@@ -121,7 +136,12 @@ public class CertSignController {
         }
     }
 
-    @PostMapping(consumes = "multipart/form-data", value = "/cert-sign")
+    @PostMapping(
+            consumes = {
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                MediaType.APPLICATION_FORM_URLENCODED_VALUE
+            },
+            value = "/cert-sign")
     @Operation(
             summary = "Sign PDF with a Digital Certificate",
             description =
@@ -137,12 +157,12 @@ public class CertSignController {
         MultipartFile p12File = request.getP12File();
         MultipartFile jksfile = request.getJksFile();
         String password = request.getPassword();
-        Boolean showSignature = request.isShowSignature();
+        Boolean showSignature = request.getShowSignature();
         String reason = request.getReason();
         String location = request.getLocation();
         String name = request.getName();
-        Integer pageNumber = request.getPageNumber() - 1;
-        Boolean showLogo = request.isShowLogo();
+        Integer pageNumber = request.getPageNumber() != null ? (request.getPageNumber() - 1) : null;
+        Boolean showLogo = request.getShowLogo();
 
         if (certType == null) {
             throw new IllegalArgumentException("Cert type must be provided");
@@ -279,7 +299,7 @@ public class CertSignController {
                 widget.setAppearance(appearance);
 
                 try (PDPageContentStream cs = new PDPageContentStream(doc, appearanceStream)) {
-                    if (showLogo) {
+                    if (Boolean.TRUE.equals(showLogo)) {
                         cs.saveGraphicsState();
                         PDExtendedGraphicsState extState = new PDExtendedGraphicsState();
                         extState.setBlendMode(BlendMode.MULTIPLY);
