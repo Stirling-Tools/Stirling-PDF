eed024d0efd7bd7a071fa6f646af68727f30c90f
Dario Ghunney Ware <dariogware@gmail.com>
2025-05-09 16:48:48 +0100

    moving security package and relevant files over to proprietary

---
+++ proprietary/src/main/java/stirling/software/proprietary/security/model/api/user/UpdateUserUsername.java
@@ -0,0 +1,14 @@
+package stirling.software.proprietary.security.model.api.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class UpdateUserUsername extends UsernameAndPass {
+
+    @Schema(description = "new username for user")
+    private String newUsername;
+}
