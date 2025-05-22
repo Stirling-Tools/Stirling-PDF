83f33f83e003ce57c6e73b61851a7e22e697aa45
Dario Ghunney Ware <dariogware@gmail.com>
2025-05-22 11:41:03 +0100

    new YamlConfig class

--- common/src/main/java/stirling/software/common/configuration/YamlPropertySourceFactory.java
+++ common/src/main/java/stirling/software/common/configuration/YamlPropertySourceFactory.java
@@ -1,8 +1,6 @@
 package stirling.software.common.configuration;
 
-import java.io.IOException;
 import java.util.Properties;
-
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
@@ -12,8 +10,7 @@ import org.springframework.core.io.support.PropertySourceFactory;
 public class YamlPropertySourceFactory implements PropertySourceFactory {
 
     @Override
-    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource)
-            throws IOException {
+    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) {
         YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
         factory.setResources(encodedResource.getResource());
         Properties properties = factory.getObject();
