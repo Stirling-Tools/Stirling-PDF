6235821942288d3cd5878d9ca2b20a6fb32e11ec
Balázs Szücs <bszucs1209@gmail.com>
2025-05-28 21:40:03 +0200

    minor fix: update package references in imports for consistency

--- src/main/java/stirling/software/SPDF/controller/api/misc/FakeScanController.java
+++ src/main/java/stirling/software/SPDF/controller/api/misc/FakeScanController.java
@@ -34,8 +34,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import stirling.software.SPDF.model.api.misc.FakeScanRequest;
-import stirling.software.SPDF.service.CustomPDFDocumentFactory;
-import stirling.software.SPDF.utils.WebResponseUtils;
+import stirling.software.common.service.CustomPDFDocumentFactory;
+import stirling.software.common.util.WebResponseUtils;
 
 @RestController
 @RequestMapping("/api/v1/misc")
