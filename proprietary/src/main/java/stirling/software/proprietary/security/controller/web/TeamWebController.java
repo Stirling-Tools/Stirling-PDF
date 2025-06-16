7e0046d6bb2eb0d2dcb93aaf64a10905411bcbf8
Ludy87 <Ludy87@users.noreply.github.com>
2025-06-16 19:29:35 +0200

    Update TeamWebController.java

--- proprietary/src/main/java/stirling/software/proprietary/security/controller/web/TeamWebController.java
+++ proprietary/src/main/java/stirling/software/proprietary/security/controller/web/TeamWebController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
