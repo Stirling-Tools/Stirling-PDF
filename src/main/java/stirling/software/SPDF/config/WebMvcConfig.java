4c5ddd6cce6f3c43bb70adfb530b428e6f57d069
Connor Yoh <connor@stirlingpdf.com>
2025-07-01 16:21:33 +0100

    Make cors policy dependent orn tauri mode

--- src/main/java/stirling/software/SPDF/config/WebMvcConfig.java
+++ src/main/java/stirling/software/SPDF/config/WebMvcConfig.java
@@ -30,15 +30,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://localhost:5173",
-                        "http://tauri.localhost",
-                        "tauri://localhost",
-                        "app://localhost",
-                        "file://")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+        if (Boolean.parseBoolean(System.getProperty("STIRLING_PDF_TAURI_MODE", "false"))) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173", "http://tauri.localhost")
+                    .allowedMethods("*")
+                    .allowedHeaders("*");
+        }
     }
 }
