5a1bdfc1c3a3bb526c24a1157d0479ab726d6f8d
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2025-07-07 23:42:23 +0100

    Update download-security-jar.sh

--- scripts/download-security-jar.sh
+++ scripts/download-security-jar.sh
@@ -1,6 +1,6 @@
 echo "Running Stirling PDF with DISABLE_ADDITIONAL_FEATURES=${DISABLE_ADDITIONAL_FEATURES} and VERSION_TAG=${VERSION_TAG}"
 # Check for DISABLE_ADDITIONAL_FEATURES and download the appropriate JAR if required
-if [ "$DISABLE_ADDITIONAL_FEATURES" = "false" ] && [ "$VERSION_TAG" != "alpha" ]; then
+if { [ "$DISABLE_ADDITIONAL_FEATURES" = "false" ] || [ "$DOCKER_ENABLE_SECURITY" = "true" ]; } && [ "$VERSION_TAG" != "alpha" ]; then
     if [ ! -f app-security.jar ]; then
         echo "Trying to download from: https://files.stirlingpdf.com/v$VERSION_TAG/Stirling-PDF-with-login.jar"
         curl -L -o app-security.jar https://files.stirlingpdf.com/v$VERSION_TAG/Stirling-PDF-with-login.jar
