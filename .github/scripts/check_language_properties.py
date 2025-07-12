1a4a22aadb1b1c79230d9aff4df9c4f66b550e35
Ludy87 <Ludy87@users.noreply.github.com>
2025-07-12 21:08:10 +0200

    Update check_language_properties.py

--- .github/scripts/check_language_properties.py
+++ .github/scripts/check_language_properties.py
@@ -329,7 +329,7 @@ def check_for_differences(reference_file, file_list, branch, actor):
         report.append("## ❌ Overall Check Status: **_Failed_**")
         report.append("")
         report.append(
-            f"@{actor} please check your translation if it conforms to the standard. Follow the format of [messages_en_GB.properties](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/stirling-pdf/src/main/resources/messages_en_GB.properties)"
+            f"@{actor} please check your translation if it conforms to the standard. Follow the format of [messages_en_GB.properties](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/app/core/src/main/resources/messages_en_GB.properties)"
         )
     else:
         report.append("## ✅ Overall Check Status: **_Success_**")
