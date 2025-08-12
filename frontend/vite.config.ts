aad04181e3eb2c930cadf25ac6755e5ba1f9748e
Anthony Stirling <77850077+Frooodle@users.noreply.github.com.>
2025-08-12 21:35:56 +0100

    remove

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow access from any IP
-    port: 5173,
-    allowedHosts: ['pdf2.froodleplex.com', 'localhost', '.local'], // Add your domain
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
