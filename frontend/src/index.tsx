67d456cb86fa81118316b6cec4ac52ac720457c9
Connor Yoh <connor@stirlingpdf.com>
2025-08-27 12:43:42 +0100

    PostHog

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import '../vite-env.d.ts';
 import './index.css'; // Import Tailwind CSS
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -6,6 +7,7 @@ import { ColorSchemeScript } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './i18n'; // Initialize i18next
+import { PostHogProvider } from 'posthog-js/react';
 
 // Compute initial color scheme
 function getInitialScheme(): 'light' | 'dark' {
@@ -27,9 +29,19 @@ const root = ReactDOM.createRoot(container); // Finds the root DOM element
 root.render(
   <React.StrictMode>
     <ColorSchemeScript defaultColorScheme={getInitialScheme()} />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+        debug: import.meta.env.MODE === 'development',
+      }}
+    >
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </PostHogProvider>
   </React.StrictMode>
 );
 
