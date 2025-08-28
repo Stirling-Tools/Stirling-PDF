442b373ff4c81d5b694c55b80b251f34b606357d
James Brunton <jbrunton96@gmail.com>
2025-08-28 10:59:38 +0100

    V2 reduce tool boilerplate (#4313)
    # Description of Changes
    Reduce boilerplate in tool frontends by creating a base frontend hook
    for the simple tools to use.
    
    I've done all the simple tools here. It'd be nice to add in some of the
    more complex tools as well in the future if we can figure out how.

--- frontend/src/tools/RemovePassword.tsx
+++ frontend/src/tools/RemovePassword.tsx
@@ -1,95 +1,55 @@
-import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { useEndpointEnabled } from "../hooks/useEndpointConfig";
-import { useFileSelection } from "../contexts/FileContext";
-import { useNavigationActions } from "../contexts/NavigationContext";
-
 import { createToolFlow } from "../components/tools/shared/createToolFlow";
-
 import RemovePasswordSettings from "../components/tools/removePassword/RemovePasswordSettings";
-
 import { useRemovePasswordParameters } from "../hooks/tools/removePassword/useRemovePasswordParameters";
 import { useRemovePasswordOperation } from "../hooks/tools/removePassword/useRemovePasswordOperation";
 import { useRemovePasswordTips } from "../components/tooltips/useRemovePasswordTips";
+import { useBaseTool } from "../hooks/tools/shared/useBaseTool";
 import { BaseToolProps, ToolComponent } from "../types/tool";
 
-const RemovePassword = ({ onPreviewFile, onComplete, onError }: BaseToolProps) => {
+const RemovePassword = (props: BaseToolProps) => {
   const { t } = useTranslation();
-  const { actions } = useNavigationActions();
-  const { selectedFiles } = useFileSelection();
-
-  const removePasswordParams = useRemovePasswordParameters();
-  const removePasswordOperation = useRemovePasswordOperation();
   const removePasswordTips = useRemovePasswordTips();
 
-  // Endpoint validation
-  const { enabled: endpointEnabled, loading: endpointLoading } = useEndpointEnabled(removePasswordParams.getEndpointName());
-
-
-  useEffect(() => {
-    removePasswordOperation.resetResults();
-    onPreviewFile?.(null);
-  }, [removePasswordParams.parameters]);
-
-  const handleRemovePassword = async () => {
-    try {
-      await removePasswordOperation.executeOperation(removePasswordParams.parameters, selectedFiles);
-      if (removePasswordOperation.files && onComplete) {
-        onComplete(removePasswordOperation.files);
-      }
-    } catch (error) {
-      if (onError) {
-        onError(error instanceof Error ? error.message : t("removePassword.error.failed", "Remove password operation failed"));
-      }
-    }
-  };
-
-  const handleThumbnailClick = (file: File) => {
-    onPreviewFile?.(file);
-    sessionStorage.setItem("previousMode", "removePassword");
-  };
-
-  const handleSettingsReset = () => {
-    removePasswordOperation.resetResults();
-    onPreviewFile?.(null);
-  };
-
-  const hasFiles = selectedFiles.length > 0;
-  const hasResults = removePasswordOperation.files.length > 0 || removePasswordOperation.downloadUrl !== null;
-  const passwordCollapsed = !hasFiles || hasResults;
+  const base = useBaseTool(
+    'removePassword',
+    useRemovePasswordParameters,
+    useRemovePasswordOperation,
+    props
+  );
 
   return createToolFlow({
     files: {
-      selectedFiles,
-      isCollapsed: hasResults,
+      selectedFiles: base.selectedFiles,
+      isCollapsed: base.hasResults,
     },
     steps: [
       {
         title: t("removePassword.password.stepTitle", "Remove Password"),
-        isCollapsed: passwordCollapsed,
-        onCollapsedClick: hasResults ? handleSettingsReset : undefined,
+        isCollapsed: base.settingsCollapsed,
+        onCollapsedClick: base.hasResults ? base.handleSettingsReset : undefined,
         tooltip: removePasswordTips,
         content: (
           <RemovePasswordSettings
-            parameters={removePasswordParams.parameters}
-            onParameterChange={removePasswordParams.updateParameter}
-            disabled={endpointLoading}
+            parameters={base.params.parameters}
+            onParameterChange={base.params.updateParameter}
+            disabled={base.endpointLoading}
           />
         ),
       },
     ],
     executeButton: {
       text: t("removePassword.submit", "Remove Password"),
-      isVisible: !hasResults,
+      isVisible: !base.hasResults,
       loadingText: t("loading"),
-      onClick: handleRemovePassword,
-      disabled: !removePasswordParams.validateParameters() || !hasFiles || !endpointEnabled,
+      onClick: base.handleExecute,
+      disabled: !base.params.validateParameters() || !base.hasFiles || !base.endpointEnabled,
     },
     review: {
-      isVisible: hasResults,
-      operation: removePasswordOperation,
+      isVisible: base.hasResults,
+      operation: base.operation,
       title: t("removePassword.results.title", "Decrypted PDFs"),
-      onFileClick: handleThumbnailClick,
+      onFileClick: base.handleThumbnailClick,
     },
   });
 };
