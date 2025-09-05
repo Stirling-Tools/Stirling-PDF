cd1fc682ab95dc076f40f193b912d14861689c6e
James Brunton <jbrunton96@gmail.com>
2025-09-05 10:58:14 +0100

    Enable ESLint no-case-declarations rule (#4348)
    # Description of Changes
    Enable ESLint [no-case-declarations
    rule](https://eslint.org/docs/latest/rules/no-case-declarations)

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -16,7 +16,6 @@ export default defineConfig(
   {
     rules: {
       "no-undef": "off", // Temporarily disabled until codebase conformant
-      "no-case-declarations": "off", // Temporarily disabled until codebase conformant
       "@typescript-eslint/ban-ts-comment": "off", // Temporarily disabled until codebase conformant
       "@typescript-eslint/no-empty-object-type": "off", // Temporarily disabled until codebase conformant
       "@typescript-eslint/no-explicit-any": "off", // Temporarily disabled until codebase conformant
