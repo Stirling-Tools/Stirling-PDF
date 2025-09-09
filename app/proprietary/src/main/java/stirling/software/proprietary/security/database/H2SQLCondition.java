ce7183d23cdfb6e9319b7033e725ba22975fc5e3
Ludy87 <Ludy87@users.noreply.github.com>
2025-09-09 22:55:29 +0200

    cleanup

--- app/proprietary/src/main/java/stirling/software/proprietary/security/database/H2SQLCondition.java
+++ app/proprietary/src/main/java/stirling/software/proprietary/security/database/H2SQLCondition.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class H2SQLCondition implements Condition {
 
     @Override
@@ -16,10 +13,8 @@ public class H2SQLCondition implements Condition {
                 env.getProperty("system.datasource.enableCustomDatabase", Boolean.class, false);
 
         if (!enableCustomDatabase) {
-            log.info("Custom database is not enabled; enabling H2-specific beans.");
             return true;
         }
-        log.info("Custom database is enabled; skipping H2-specific beans.");
         return false;
     }
 }
