e5909466d4f8b3f3e5859cb82a7b933b38ba427a
Connor Yoh <connor@stirlingpdf.com>
2025-09-16 11:41:07 +0100

    Remove unused method option parameters

--- frontend/src/constants/splitConstants.ts
+++ frontend/src/constants/splitConstants.ts
@@ -26,68 +26,52 @@ export const isSplitMethod = (value: string | null): value is SplitMethod => {
 
 export interface MethodOption {
   method: SplitMethod;
-  icon: string;
   prefixKey: string;
   nameKey: string;
-  descKey: string;
   tooltipKey: string;
 }
 
 export const METHOD_OPTIONS: MethodOption[] = [
   {
     method: SPLIT_METHODS.BY_PAGES,
-    icon: "format-list-numbered-rounded",
     prefixKey: "split.methods.prefix.splitAt",
     nameKey: "split.methods.byPages.name",
-    descKey: "split.methods.byPages.desc",
     tooltipKey: "split.methods.byPages.tooltip"
   },
   {
     method: SPLIT_METHODS.BY_CHAPTERS,
-    icon: "bookmark-rounded",
     prefixKey: "split.methods.prefix.splitBy",
     nameKey: "split.methods.byChapters.name",
-    descKey: "split.methods.byChapters.desc",
     tooltipKey: "split.methods.byChapters.tooltip"
   },
   {
     method: SPLIT_METHODS.BY_SECTIONS,
-    icon: "grid-on-rounded",
     prefixKey: "split.methods.prefix.splitBy",
     nameKey: "split.methods.bySections.name",
-    descKey: "split.methods.bySections.desc",
     tooltipKey: "split.methods.bySections.tooltip"
   },
   {
     method: SPLIT_METHODS.BY_SIZE,
-    icon: "storage-rounded",
     prefixKey: "split.methods.prefix.splitBy",
     nameKey: "split.methods.bySize.name",
-    descKey: "split.methods.bySize.desc",
     tooltipKey: "split.methods.bySize.tooltip"
   },
   {
     method: SPLIT_METHODS.BY_PAGE_COUNT,
-    icon: "numbers-rounded",
     prefixKey: "split.methods.prefix.splitBy",
     nameKey: "split.methods.byPageCount.name",
-    descKey: "split.methods.byPageCount.desc",
     tooltipKey: "split.methods.byPageCount.tooltip"
   },
   {
     method: SPLIT_METHODS.BY_DOC_COUNT,
-    icon: "content-copy-rounded",
     prefixKey: "split.methods.prefix.splitBy",
     nameKey: "split.methods.byDocCount.name",
-    descKey: "split.methods.byDocCount.desc",
     tooltipKey: "split.methods.byDocCount.tooltip"
   },
   {
     method: SPLIT_METHODS.BY_PAGE_DIVIDER,
-    icon: "auto-awesome-rounded",
     prefixKey: "split.methods.prefix.splitBy",
     nameKey: "split.methods.byPageDivider.name",
-    descKey: "split.methods.byPageDivider.desc",
     tooltipKey: "split.methods.byPageDivider.tooltip"
   }
 ];
