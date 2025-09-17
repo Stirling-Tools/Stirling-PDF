57e0b5bf86677fda7f635bd25b33f22819eeaa2d
James Brunton <james@stirlingpdf.com>
2025-09-17 09:10:32 +0100

    Remove file prefix from Merge

--- frontend/src/hooks/tools/merge/useMergeOperation.ts
+++ frontend/src/hooks/tools/merge/useMergeOperation.ts
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { useToolOperation, ResponseHandler, ToolOperationConfig, ToolType } from '../shared/useToolOperation';
+import { useToolOperation, ToolOperationConfig, ToolType } from '../shared/useToolOperation';
 import { createStandardErrorHandler } from '../../../utils/toolErrorHandler';
 import { MergeParameters } from './useMergeParameters';
 
@@ -16,19 +16,12 @@ const buildFormData = (parameters: MergeParameters, files: File[]): FormData => 
   return formData;
 };
 
-const mergeResponseHandler: ResponseHandler = (blob: Blob, originalFiles: File[]): File[] => {
-  const filename = `merged_${originalFiles[0].name}`
-  return [new File([blob], filename, { type: 'application/pdf' })];
-};
-
 // Operation configuration for automation
 export const mergeOperationConfig: ToolOperationConfig<MergeParameters> = {
   toolType: ToolType.multiFile,
   buildFormData,
   operationType: 'merge',
   endpoint: '/api/v1/general/merge-pdfs',
-  filePrefix: 'merged_',
-  responseHandler: mergeResponseHandler,
 };
 
 export const useMergeOperation = () => {
