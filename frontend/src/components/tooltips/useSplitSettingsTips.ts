38d10e7c06aaeb0d9189fd72fc531c2ac57d42bc
Ludy87 <Ludy87@users.noreply.github.com>
2025-09-24 19:50:56 +0200

    Update useSplitSettingsTips.ts

--- frontend/src/components/tooltips/useSplitSettingsTips.ts
+++ frontend/src/components/tooltips/useSplitSettingsTips.ts
@@ -1,5 +1,6 @@
 import { useMemo } from 'react';
-import { useTranslation, type TFunction } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import type { TFunction } from 'i18next';
 import { TooltipContent } from '../../types/tips';
 import { SPLIT_METHODS, type SplitMethod } from '../../constants/splitConstants';
 
