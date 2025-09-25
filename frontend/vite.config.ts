bc3bcb9c25064a7fbc6f74adade9a93ee676e615
Reece <reece@stirlingpdf.com>
2025-09-25 11:02:05 +0100

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem')
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -12,5 +17,11 @@ export default defineConfig({
       },
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2020'
+    }
+  },
+  assetsInclude: ['**/*.mjs'],
   base: process.env.RUN_SUBPATH ? `/${process.env.RUN_SUBPATH}` : './',
 });
