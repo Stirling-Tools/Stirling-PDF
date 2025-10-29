d1067d8639f695553ffc1fcbabe423d55216946a
Reece <reece@stirlingpdf.com>
2025-10-29 00:48:55 +0000

    -

--- frontend/src/core/styles/zIndex.ts
+++ frontend/src/core/styles/zIndex.ts
@@ -10,6 +10,7 @@ export const Z_INDEX_OVER_FILE_MANAGER_MODAL = 1300;
 
 export const Z_INDEX_AUTOMATE_MODAL = 1100;
 
+// page editor Zindexes
 export const Z_INDEX_HOVER_ACTION_MENU = 100;
 export const Z_INDEX_SELECTION_BOX = 1000;
 export const Z_INDEX_DROP_INDICATOR = 1001;
