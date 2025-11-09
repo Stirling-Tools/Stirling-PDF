410d6e0742d8e287d1282446d0d9d99df5bdbbd0
Reece <reece@stirlingpdf.com>
2025-11-09 18:34:58 +0000

    -

--- frontend/src/core/contexts/viewer/viewerBridges.ts
+++ frontend/src/core/contexts/viewer/viewerBridges.ts
@@ -17,6 +17,7 @@ export interface PanAPIWrapper {
   enable: () => void;
   disable: () => void;
   toggle: () => void;
+  makePanDefault: () => void;
 }
 
 export interface SelectionAPIWrapper {
@@ -29,6 +30,7 @@ export interface SpreadAPIWrapper {
   setSpreadMode: (mode: SpreadMode) => void;
   getSpreadMode: () => SpreadMode | null;
   toggleSpreadMode: () => void;
+  SpreadMode: typeof SpreadMode;
 }
 
 export interface RotationAPIWrapper {
@@ -43,6 +45,7 @@ export interface SearchAPIWrapper {
   clear: () => void;
   next: () => void;
   previous: () => void;
+  goToResult: (index: number) => void;
 }
 
 export interface ThumbnailAPIWrapper {
