47ebe7fc4618ead982107ef75da1420a4aeeaab8
Balázs Szücs <bszucs1209@gmail.com>
2025-11-17 17:25:30 +0100

    fix import from git merge conflict mistake
    Signed-off-by: Balázs Szücs <bszucs1209@gmail.com>

--- app/core/src/main/java/stirling/software/SPDF/controller/api/SplitPDFController.java
+++ app/core/src/main/java/stirling/software/SPDF/controller/api/SplitPDFController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
