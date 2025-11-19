62011a969762d4a7be98f56155d7d1fb7ee79fa7
OUNZAR Aymane <aymane.ounzar@imt-atlantique.net>
2025-11-19 15:28:32 +0100

    Define request schema for ConvertHtmlToPDFWebBased controller

---
+++ app/common/src/main/java/stirling/software/common/model/api/converters/HTMLToPdfWebBasedRequest.java
@@ -0,0 +1,10 @@
+package stirling.software.common.model.api.converters;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import stirling.software.common.model.api.PDFFile;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class HTMLToPdfRequestWebBased extends PDFFile {}
