717e39e2d89952c02d2a50a525a8767b5517b814
Ludy87 <Ludy87@users.noreply.github.com>
2025-11-23 18:48:15 +0100

    Update swagger_endpoint_test.py

--- testing/swagger_endpoint_test.py
+++ testing/swagger_endpoint_test.py
@@ -125,7 +125,7 @@ DEFAULT_SKIP_ENDPOINTS = {
     ("get", "/api/v1/admin/settings/section/{sectionName}"),
     ("get", "/api/v1/admin/settings/key/{key}"),
     ("get", "/api/v1/admin/settings/delta"),
-    ("get", "/api/v1/admin/settings")
+    ("get", "/api/v1/admin/settings"),
     # Could not find the Qt platform plugin headless error
     ("post", "/api/v1/ebook/pdf"),
 }
