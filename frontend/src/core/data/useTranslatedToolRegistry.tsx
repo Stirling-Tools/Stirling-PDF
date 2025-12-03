ff0ada2b704c1221a2db37a41694497fcc42f9ec
EthanHealy01 <ethan.healy.21@gmail.com>
2025-12-03 00:50:56 +0000

    adding white space to re-trigger the useTranslatedToolRegistry.tsx update for max files

--- frontend/src/core/data/useTranslatedToolRegistry.tsx
+++ frontend/src/core/data/useTranslatedToolRegistry.tsx
@@ -333,7 +333,6 @@ export function useTranslatedToolCatalog(): TranslatedToolCatalog {
         supportsAutomate: false,
         automationSettings: null,
         maxFiles: 1,
-        
       },
       validateSignature: {
         icon: <LocalIcon icon="verified-rounded" width="1.5rem" height="1.5rem" />,
