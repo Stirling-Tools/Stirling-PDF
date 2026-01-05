a83f494af113938f237a62e7dfd42937d25500c8
Ludy87 <Ludy87@users.noreply.github.com>
2026-01-05 16:22:30 +0100

    Refactor converter requests to extend GeneralFile
    Changed EmlToPdfRequest and HTMLToPdfRequest to extend GeneralFile instead of PDFFile. This improves code reuse and aligns the request models with a more general file abstraction.

--- app/common/src/main/java/stirling/software/common/model/api/converters/HTMLToPdfRequest.java
+++ app/common/src/main/java/stirling/software/common/model/api/converters/HTMLToPdfRequest.java
@@ -5,11 +5,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import stirling.software.common.model.api.PDFFile;
+import stirling.software.common.model.api.GeneralFile;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class HTMLToPdfRequest extends PDFFile {
+public class HTMLToPdfRequest extends GeneralFile {
 
     @Schema(
             description = "Zoom level for displaying the website. Default is '1'.",
