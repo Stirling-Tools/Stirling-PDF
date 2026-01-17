0cfc872880ac36646d279d1149e284aa76e468a3
Balázs Szücs <127139797+balazs-szucs@users.noreply.github.com>
2026-01-17 20:44:02 +0100

    feat(pipeline): improve file processing with resource management and temp file handling (#5488)
    # Description of Changes
    
    <!--
    Please provide a summary of the changes, including:
    
    - What was changed
    - Why the change was made
    - Any challenges encountered
    
    Closes #(issue_number)
    -->
    
    ---
    
    ## Checklist
    
    ### General
    
    - [ ] I have read the [Contribution
    Guidelines](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/CONTRIBUTING.md)
    - [ ] I have read the [Stirling-PDF Developer
    Guide](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/devGuide/DeveloperGuide.md)
    (if applicable)
    - [ ] I have read the [How to add new languages to
    Stirling-PDF](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/devGuide/HowToAddNewLanguage.md)
    (if applicable)
    - [ ] I have performed a self-review of my own code
    - [ ] My changes generate no new warnings
    
    ### Documentation
    
    - [ ] I have updated relevant docs on [Stirling-PDF's doc
    repo](https://github.com/Stirling-Tools/Stirling-Tools.github.io/blob/main/docs/)
    (if functionality has heavily changed)
    - [ ] I have read the section [Add New Translation
    Tags](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/devGuide/HowToAddNewLanguage.md#add-new-translation-tags)
    (for new translation tags only)
    
    ### Translations (if applicable)
    
    - [ ] I ran
    [`scripts/counter_translation.py`](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/docs/counter_translation.md)
    
    ### UI Changes (if applicable)
    
    - [ ] Screenshots or videos demonstrating the UI changes are attached
    (e.g., as comments or direct attachments in the PR)
    
    ### Testing (if applicable)
    
    - [ ] I have tested my changes locally. Refer to the [Testing
    Guide](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/devGuide/DeveloperGuide.md#6-testing)
    for more details.
    
    Signed-off-by: Balázs Szücs <bszucs1209@gmail.com>

--- app/core/src/main/java/stirling/software/SPDF/model/PipelineResult.java
+++ app/core/src/main/java/stirling/software/SPDF/model/PipelineResult.java
@@ -1,14 +1,37 @@
 package stirling.software.SPDF.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.core.io.Resource;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import stirling.software.common.util.TempFile;
 
 @Data
-public class PipelineResult {
+@Slf4j
+public class PipelineResult implements AutoCloseable {
     private List<Resource> outputFiles;
     private boolean hasErrors;
     private boolean filtersApplied;
+    private List<TempFile> tempFiles = new ArrayList<>();
+
+    public void addTempFile(TempFile tempFile) {
+        tempFiles.add(tempFile);
+    }
+
+    @Override
+    public void close() {
+        for (TempFile file : tempFiles) {
+            file.close();
+            log.debug("Deleted temp file: {}", file.getAbsolutePath());
+        }
+        tempFiles.clear();
+    }
+
+    public void cleanup() {
+        close();
+    }
 }
