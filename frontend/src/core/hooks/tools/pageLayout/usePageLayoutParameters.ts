5c9c33b1b5e1d229f512cb891bd1d478b0390135
OUNZAR Aymane <aymane.ounzar@imt-atlantique.net>
2026-03-06 10:55:47 +0100

    fix(types): standardize quotation marks in PageLayoutParameters interface

--- frontend/src/core/hooks/tools/pageLayout/usePageLayoutParameters.ts
+++ frontend/src/core/hooks/tools/pageLayout/usePageLayoutParameters.ts
@@ -2,13 +2,13 @@ import { BaseParameters } from '@app/types/parameters';
 import { useBaseParameters, BaseParametersHook } from '@app/hooks/tools/shared/useBaseParameters';
 
 export interface PageLayoutParameters extends BaseParameters {
-  mode: "DEFAULT" | "CUSTOM";
+  mode: 'DEFAULT' | 'CUSTOM';
   pagesPerSheet: number;
   rows: number;
   cols: number;
-  orientation: "PORTRAIT" | "LANDSCAPE"
-  arrangement: "BY_COLUMNS" | "BY_ROWS"
-  readingDirection: "LTR" | "RTL"
+  orientation: 'PORTRAIT' | 'LANDSCAPE';
+  arrangement: 'BY_COLUMNS' | 'BY_ROWS';
+  readingDirection: 'LTR' | 'RTL';
   innerMargin?: number;
   topMargin?: number;
   bottomMargin?: number;
@@ -19,13 +19,13 @@ export interface PageLayoutParameters extends BaseParameters {
 }
 
 export const defaultParameters: PageLayoutParameters = {
-  mode: "DEFAULT",
+  mode: 'DEFAULT',
   pagesPerSheet: 4,
   rows: 1,
   cols: 1,
-  orientation: "PORTRAIT",
-  arrangement: "BY_ROWS",
-  readingDirection: "LTR",
+  orientation: 'PORTRAIT',
+  arrangement: 'BY_COLUMNS',
+  readingDirection: 'LTR',
   addBorder: false,
   innerMargin: 0,
   topMargin: 0,
