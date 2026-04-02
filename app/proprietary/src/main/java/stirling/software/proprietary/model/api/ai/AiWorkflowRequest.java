0c3dca0fe000255ec42602b7a5300216bda47664
James Brunton <jbrunton96@gmail.com>
2026-04-02 13:49:59 +0100

    Allow empty list inputs to AI

--- app/proprietary/src/main/java/stirling/software/proprietary/model/api/ai/AiWorkflowRequest.java
+++ app/proprietary/src/main/java/stirling/software/proprietary/model/api/ai/AiWorkflowRequest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import lombok.Data;
@@ -15,7 +14,6 @@ import lombok.Data;
 public class AiWorkflowRequest {
 
     @NotNull
-    @NotEmpty
     @Schema(description = "The input PDF files")
     private List<AiWorkflowFileInput> fileInputs;
 
