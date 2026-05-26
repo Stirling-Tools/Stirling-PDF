0b13441f03813248b7e64625b49bce5722783697
Anthony Stirling <anthony@stirlingpdf.com>
2026-05-26 19:47:15 +0100

    Send orientation to backend + tests

---
+++ frontend/editor/src/core/hooks/tools/adjustPageScale/adjustPageScaleFormData.ts
@@ -0,0 +1,15 @@
+import {
+  AdjustPageScaleParameters,
+} from "@app/hooks/tools/adjustPageScale/useAdjustPageScaleParameters";
+
+export const buildAdjustPageScaleFormData = (
+  parameters: AdjustPageScaleParameters,
+  file: File,
+): FormData => {
+  const formData = new FormData();
+  formData.append("fileInput", file);
+  formData.append("scaleFactor", parameters.scaleFactor.toString());
+  formData.append("pageSize", parameters.pageSize);
+  formData.append("orientation", parameters.orientation);
+  return formData;
+};
