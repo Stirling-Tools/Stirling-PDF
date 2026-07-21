ae909a835cf488d3720324ae3da471eb2483ad97
Balázs Szücs <bszucs1209@gmail.com>
2026-07-21 18:36:49 +0200

    chore: resolve compilation errors from bad merge again

--- app/proprietary/src/main/java/stirling/software/proprietary/storage/provider/LocalStorageProvider.java
+++ app/proprietary/src/main/java/stirling/software/proprietary/storage/provider/LocalStorageProvider.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 import java.util.UUID;
