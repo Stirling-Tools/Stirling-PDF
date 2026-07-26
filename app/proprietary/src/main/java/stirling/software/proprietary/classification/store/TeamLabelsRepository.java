2b3dd8b4c4ca5690e92c12fb7c5b24f5b4744570
Ludy <Ludy87@users.noreply.github.com>
2026-07-26 17:40:57 +0200

    Delete app/proprietary/src/main/java/stirling/software/proprietary/classification/store/TeamLabelsRepository.java

--- app/proprietary/src/main/java/stirling/software/proprietary/classification/store/TeamLabelsRepository.java
+++
@@ -1,5 +0,0 @@
-package stirling.software.proprietary.classification.store;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamLabelsRepository extends JpaRepository<TeamLabelsEntity, Long> {}
