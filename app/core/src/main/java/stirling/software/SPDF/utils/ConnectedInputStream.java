4e901f7524b3789095cd02a76f7407a375836941
brios <127139797+balazs-szucs@users.noreply.github.com>
2026-08-09 21:41:57 +0200

    refactor(package): rename example classes and update package structure (#7400)
    # Description of Changes
    
    Refactors a small set of previously vendored/derived PDFBox "example"
    classes into Stirling’s own package namespaces.
    
    
    
    <!--
    Please provide a summary of the changes, including:
    
    - What was changed
    - Why the change was made
    - Any challenges encountered
    
    Closes #(issue_number)
    -->
    
    ---
    
    ## Checklist
    
    ### General
    
    - [X] I have read the [Contribution
    Guidelines](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/CONTRIBUTING.md)
    - [X] I have read the [Stirling-PDF Developer
    Guide](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/DeveloperGuide.md)
    (if applicable)
    - [ ] I have read the [How to add new languages to
    Stirling-PDF](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/devGuide/HowToAddNewLanguage.md)
    (if applicable)
    - [X] I have performed a self-review of my own code
    - [X] My changes generate no new warnings
    
    ### Documentation
    
    - [ ] I have updated relevant docs on [Stirling-PDF's doc
    repo](https://github.com/Stirling-Tools/Stirling-Tools.github.io/blob/main/docs/)
    (if functionality has heavily changed)
    - [ ] I have read the section [Add New Translation
    Tags](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/devGuide/HowToAddNewLanguage.md#add-new-translation-tags)
    (for new translation tags only)
    
    ### Translations (if applicable)
    
    - [ ] I ran
    [`scripts/counter_translation.py`](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/docs/counter_translation.md)
    
    ### UI Changes (if applicable)
    
    - [ ] Screenshots or videos demonstrating the UI changes are attached
    (e.g., as comments or direct attachments in the PR)
    
    ### Testing (if applicable)
    
    - [X] I have run `task check` to verify linters, typechecks, and tests
    pass
    - [X] I have tested my changes locally. Refer to the [Testing
    Guide](https://github.com/Stirling-Tools/Stirling-PDF/blob/main/DeveloperGuide.md#7-testing)
    for more details.

--- app/core/src/main/java/stirling/software/SPDF/utils/ConnectedInputStream.java
+++ app/core/src/main/java/stirling/software/SPDF/utils/ConnectedInputStream.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.pdfbox.examples.util;
+package stirling.software.SPDF.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
