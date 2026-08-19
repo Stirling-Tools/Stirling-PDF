dae216c64b7d6449192dea6354ef447dd9965ca7
James Brunton <james@stirlingpdf.com>
2026-08-19 16:10:46 +0100

    Make documents query available to all Processor users and remove access to Audit Tab to non-enterprise users

---
+++ app/proprietary/src/main/java/stirling/software/proprietary/audit/PortalDocumentsScopeResolver.java
@@ -0,0 +1,9 @@
+package stirling.software.proprietary.audit;
+
+/**
+ * Resolves which slice of the documents queue a portal user may see.
+ */
+public interface PortalDocumentsScopeResolver {
+
+    PortalAuditScope resolve();
+}
