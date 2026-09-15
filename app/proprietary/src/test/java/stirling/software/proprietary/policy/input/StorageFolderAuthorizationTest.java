d9319811441719a1045642c8774d87ba1f2d9725
Anthony Stirling <77850077+Frooodle@users.noreply.github.com>
2026-09-16 00:33:18 +0100

    Pass the output sinks list in the storage folder authorization test

--- app/proprietary/src/test/java/stirling/software/proprietary/policy/input/StorageFolderAuthorizationTest.java
+++ app/proprietary/src/test/java/stirling/software/proprietary/policy/input/StorageFolderAuthorizationTest.java
@@ -146,7 +146,8 @@ class StorageFolderAuthorizationTest {
                         authority,
                         mock(PolicyTriggerManager.class),
                         properties,
-                        List.of(input));
+                        List.of(input),
+                        List.of());
         runner =
                 new PolicyRunner(
                         engine,
